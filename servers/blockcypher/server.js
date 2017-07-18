add5373dbecddf5df4fb0ae196dc96a14ad21496
Steven Zeiler <zeiler.steven@gmail.com>
2017-07-18 11:48:32 -0400

    amqp logging and crash on disconnect

--- servers/blockcypher/server.js
+++ servers/blockcypher/server.js
@@ -1,6 +1,6 @@
-
 const Hapi = require("hapi");
 const amqp = require("amqplib");
+const log = require("winston");
 
 const AMQP_URL = "amqp://blockcypher.anypay.global";
 const QUEUE = "blockcypher:webhooks";
@@ -13,34 +13,64 @@ server.connection({
 });
 
 amqp.connect(AMQP_URL).then(conn => {
-	console.log('amqp:connected', AMQP_URL);
+
+	log.info('amqp:connected', AMQP_URL);
+
+  conn.on("error", error => {
+    log.error('amqp:connection:error', error);
+    process.exit(1);
+  });
+
+  conn.on("close", () => {
+    log.error('amqp:connection:closed');
+    process.exit(1);
+  });
 
 	return conn.createChannel().then(channel => {
-		console.log('channel:created');
+		log.info('amqp:channel:created');
+
+    channel.on("error", error => {
+      log.error('amqp:channel:error', error);
+      process.exit(1);
+    });
+
+    channel.on("close", () => {
+      log.error('amqp:channel:closed');
+      process.exit(1);
+    });
 
     server.route({
 			method: 'POST',
 			path:'/blockcypher/webhooks', 
 			handler: function (request, reply) {
 
+        log.info("blockcypher:callback", request.payload);
+
 				let message = JSON.stringify(request.payload);
 
-				channel.sendToQueue(QUEUE, new Buffer(message));
+				let sent = channel.sendToQueue(QUEUE, new Buffer(message));
 
-				reply();
+        if (!sent) {
+          log.error('amqp:send:error', message);
+          reply().code(500);
+        } else {
+          log.info('amqp:sent', message);
+          reply();
+        }
 			}
 		});
 
 		channel.assertQueue(QUEUE, {durable: true}).then(() => {
+        log.info('amqp:queue:asserted', QUEUE);
 
 				server.start((err) => {
 						if (err) {
 								throw err;
 						}
-						console.log('Server running at:', server.info.uri);
+						log.info('server:started', server.info.uri);
 				});
 		});
   });
 })
-.catch(console.warn);
+.catch(log.error);
 
