0768ad33f7a0f2be5ce4bf5f9a03c70c91400221
Steven Zeiler <zeiler.steven@gmail.com>
2017-08-08 00:35:35 -0400

    push notification to amqp when instansend pays invoice

--- processes/dash_monitor/index.js
+++ processes/dash_monitor/index.js
@@ -1,5 +1,8 @@
 
 const Poller = require('./poller');
+const Invoice = require('../../lib/models/invoice');
+const AMQP_URL = 'amqp://blockcypher.anypay.global';
+const amqp = require('amqplib');
 
 Poller.start()
 
@@ -16,4 +19,40 @@ Poller.vent.on('normalsend:received', data => {
 Poller.vent.on('instantsend:received', data => {
 
   console.log('instantsend:received', data);
+
+  Invoice.findOne({ where: {address: data.address }}).then(invoice => {
+
+    if (invoice) {
+      console.log('found invoice', invoice.toJSON());
+
+      if (invoice.status === 'paid') {
+        console.log('INVOICE ALREADY PAID');
+      } else {
+        if (parseFloat(invoice.amount) === data.amount) {
+          console.log('INVOICE PAID!');
+
+          invoice.updateAttributes({
+            status: 'paid',
+            paidAt: new Date()
+          })
+          .then(() => {
+            console.log('invoice updated', invoice.uid);
+
+            amqp.connect(AMQP_URL).then(conn => {
+              console.log('connected');
+
+              conn.createChannel().then(channel => {
+                channel.sendToQueue('invoices:paid', Buffer.from(invoice.uid))
+                console.log('sent to invoices:paid', invoice.uid);
+                conn.close();
+              });
+            });
+          });
+
+        } else {
+          console.log('INVALID INVOICE AMOUNT!');
+        }
+      }
+    }
+  });
 });
