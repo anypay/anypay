3d2eb920213ee0dcb33367dcb971e140dcb6fa95
Zyler <zyler@Zylers-Mac-Pro.local>
2017-10-01 21:27:26 -0400

    bitcoin invoices with migrations

---
+++ bin/create_payout.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const DashPayout = require('../lib/models/dash_payout');
+const DashPayoutService = require('../servers/dash_payouts/lib');
+
+let accountId = process.argv[2];
+
+console.log(`Create payout for account ${accountId}`);
+
+DashPayoutService.createPayout(accountId).then(payout => {
+  console.log(`Created payout for account ${accountId}`, payout);
+})
+.catch(error => {
+  console.error(`error creating payout for account ${accountId}`);
+});
+
