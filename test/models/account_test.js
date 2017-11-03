c1835f3a0c820c723294064221084310502bba5b
Zyler <zyler@Zylers-Mac-Pro.local>
2017-11-02 23:37:18 -0400

    Automatically downcase email in database upon creation

---
+++ test/models/account_test.js
@@ -0,0 +1,36 @@
+const Account = require('../../lib/models/account');
+const assert = require('assert');
+const Chance = require('chance');
+const chance = new Chance();
+
+describe('Account Model', () => {
+
+  it('should turn email to lower case when creating new account', done => {
+
+    let email = chance.email().toUpperCase();
+
+    let downcasedEmail = email.toLowerCase();
+
+    Account.create({
+      email: email
+    })
+    .then(account => {
+      console.log('original email', email);
+      console.log('saved email', downcasedEmail);
+      assert.strictEqual(account.email, downcasedEmail);
+      done();
+    });
+  });
+
+  it('should automatically generate a uid', done => {
+
+    Account.create({
+      email: chance.email()
+    })
+    .then(account => {
+      assert(account.uid);
+      done();
+    });
+  });
+});
+
