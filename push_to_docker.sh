51e3920529fb16d6add029280b4018d60b0bfe9e
Steven Zeiler <me@stevezeiler.com>
2017-12-04 17:38:57 -0500

    remove docker email from login

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -1,6 +1,6 @@
-docker login --email $DOCKER_EMAIL \
-             --username $DOCKER_USER \
-             --password $DOCKER_PASSWORD 
+docker login \
+ --username $DOCKER_USER \
+ --password $DOCKER_PASSWORD 
 
 docker tag paymentservice_rest-api anypay/paymentservice_rest-api:latest
 docker push anypay/paymentservice_rest-api:latest
