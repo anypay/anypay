1d69c299b5dac6fdd72db42977846da9a13d8f4b
Steven Zeiler <me@stevezeiler.com>
2017-12-04 21:28:12 -0500

    do not use docker email when pushing

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -1,6 +1,4 @@
-docker login \
- --username $DOCKER_USER \
- --password $DOCKER_PASSWORD 
+docker login --username $DOCKER_USER --password $DOCKER_PASSWORD 
 
 docker tag paymentservice_rest-api anypay/paymentservice_rest-api:latest
 docker push anypay/paymentservice_rest-api:latest
