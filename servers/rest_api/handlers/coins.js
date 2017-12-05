372cbef6e91f5d93b37dbb25c0d56f66d94d84d6
Steven Zeiler <me@stevezeiler.com>
2017-12-04 22:19:31 -0500

    remove merge conflict

--- servers/rest_api/handlers/coins.js
+++ servers/rest_api/handlers/coins.js
@@ -46,7 +46,6 @@ function CoinsFromAccount(account) {
 
   // Ensure extra flag for beta release of litecoin
   // TODO Remove the litecoin_enabled account flag once fully supported
-<<<<<<< HEAD
   if (account.litecoin_address && account.litecoin_enabled) {
     coins['LTC'].enabled = true;
   }
@@ -55,12 +54,6 @@ function CoinsFromAccount(account) {
     coins['DOGE'].enabled = true;
   }
 
-=======
-  if (account.litecoin_cash_address && account.litecoin_enabled) {
-    coins['LTC'].enabled = true;
-  }
-
->>>>>>> 45c8a55102f3e63d32e6c74f3162bc8be11d658f
   return coins;
 }
 
