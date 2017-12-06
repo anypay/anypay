e10451b94f267acb150d8ad996bffa821cd6c5f3
Brett Wraight <brett@golabs.com>
2017-12-06 10:06:02 +1000

    Create a local postgres database and run migrations

--- servers/rest_api/handlers/invoices.js
+++ servers/rest_api/handlers/invoices.js
@@ -1,12 +1,11 @@
-const Invoice = require('../../../lib/models/invoice');
+const Invoice=require('../../../lib/models/invoice');
+        module.exports.index=(request, reply)=>{
 
-module.exports.index = (request, reply) => {
-
-  Invoice.findAll({ where: {
-    account_id: request.auth.credentials.accessToken.account_id
-  }})
-  .then(invoices => {
-    reply({ invoices: invoices });
-  });
-}
+Invoice.findAll({ where: {
+account_id: request.auth.credentials.accessToken.account_id
+}})
+        .then(invoices=>{
+        reply({ invoices: invoices });
+        });
+        }
 
