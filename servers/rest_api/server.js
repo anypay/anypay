6411954e02f3a3744ef9363075c2fbf110749dbc
Steven Zeiler <me@stevezeiler.com>
2017-12-07 15:37:43 -0500

    prepare to serve bitcoin-cash/invoices

--- servers/rest_api/server.js
+++ servers/rest_api/server.js
@@ -180,7 +180,7 @@ server.register(Basic, err => {
 
     server.route({
       method: "POST",
-      path: "/bitcoin_cash/invoices",
+      path: "/bitcoin-cash/invoices",
       config: {
         auth: "token",
         handler: BitcoinCashInvoicesController.create
