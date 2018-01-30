3ab995c2ec3e89fa9a203fac3c5f99df653c84a6
Mike Hearn <mike@Mikes-iMac.local>
2018-01-30 18:22:19 -0500

    fix spacing format

--- lib/payment_processor.ts
+++ lib/payment_processor.ts
@@ -3,68 +3,68 @@ import {Payment} from '../types/interfaces';
 
 export async function handlePayment(invoice, payment: Payment) {
 
-	if (invoice.amount === payment.amount) {
+  if (invoice.amount === payment.amount) {
 
-		return handlePaid(invoice, payment);
+    return handlePaid(invoice, payment);
 
-	} else if (payment.amount < invoice.amount) {
+  } else if (payment.amount < invoice.amount) {
 
-		return handleUnderpaid(invoice, payment);
+    return handleUnderpaid(invoice, payment);
 
-	} else if (payment.amount > invoice.amount) {
+  } else if (payment.amount > invoice.amount) {
 
-		return handleOverpaid(invoice, payment);
-	}
+   return handleOverpaid(invoice, payment);
+  }
 }
 
 export async function handleUnderpaid(invoice, payment: Payment) {
 
-	if (payment.amount >= invoice.amount) {
-		throw new Error('underpaid handler called with sufficient payment');
-	}
+  if (payment.amount >= invoice.amount) {
+    throw new Error('underpaid handler called with sufficient payment');
+  }
 
-	var invoice = await invoice.updateAttributes({
-		amount_paid: payment.amount,
-		hash: payment.hash,
-		status: 'underpaid',
-		paidAt: new Date()
-	})
+  var invoice = await invoice.updateAttributes({
+    amount_paid: payment.amount,
+    hash: payment.hash,
+    status: 'underpaid',
+    paidAt: new Date()
+  })
 
-	return invoice;
+  return invoice;
 }
 
 export async function handlePaid(invoice, payment: Payment) {
 
-	if (payment.amount >= invoice.amount) {
-		throw new Error('underpaid handler called with sufficient payment');
-	}
+  if (payment.amount >= invoice.amount) {
+    throw new Error('underpaid handler called with sufficient payment');
+  }
 
-	var invoice = await invoice.updateAttributes({
-		amount_paid: payment.amount,
-		hash: payment.hash,
-		status: 'paid',
-		paidAt: new Date()
-	})
+  var invoice = await invoice.updateAttributes({
+    amount_paid: payment.amount,
+    hash: payment.hash,
+    status: 'paid',
+    paidAt: new Date()
+  })
 
-	return invoice;
+  return invoice;
 }
 
 export async function handleOverpaid(invoice, payment: Payment) {
 
-	if (payment.amount <= invoice.amount) {
-		throw new Error('overpaid handler called with insufficient payment');
-	}
-	if (payment.amount === invoice.amount) {
-		throw new Error('overpaid handler called with exactly sufficient payment');
-	}
+  if (payment.amount <= invoice.amount) {
+    throw new Error('overpaid handler called with insufficient payment');
+  }
+  if (payment.amount === invoice.amount) {
+    throw new Error('overpaid handler called with exactly sufficient payment');
+  }
+  
+  var invoice = await invoice.updateAttributes({
+    amount_paid: payment.amount,
+    hash: payment.hash,
+    status: 'overpaid',
+    paidAt: new Date()
+  })
 
-	var invoice = await invoice.updateAttributes({
-		amount_paid: payment.amount,
-		hash: payment.hash,
-		status: 'overpaid',
-		paidAt: new Date()
-	})
-
-	return invoice;
+  return invoice;
 }
 
