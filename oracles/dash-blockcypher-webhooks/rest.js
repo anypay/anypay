ac633c635bb03420d495a7a1ab12c2d072249279
Steven Zeiler <me@stevezeiler.com>
2018-02-02 15:46:38 -0500

    push message from oracle to amqp

--- oracles/dash-blockcypher-webhooks/rest.js
+++ oracles/dash-blockcypher-webhooks/rest.js
@@ -2,6 +2,11 @@
 const Hapi = require('hapi');
 const restPort = process.env.REST_PORT || '12334';
 const logger = require('winston');
+const amqp = require("amqplib");
+
+const AMQP_QUEUE = 'anypay:payment:received';
+
+const AMQP_URL = "amqp://blockcypher.anypay.global";
 
 // Create a server with a host and port
 const server = Hapi.server({ 
@@ -21,50 +26,73 @@ function handleBlockcypherTx(tx) {
     }
   });
 
-  outputs.map(output => {
-    logger.info('output', output);
+  outputs.map(payment => {
+    logger.info('output', payment);
+
+    let message = JSON.stringify(payment);
+
+    let sent = channel.sendToQueue(AMQP_QUEUE, new Buffer(message));
   })
 }
 
-// Add the route
-server.route({
-    method: 'POST',
-    path:'/oracles/dash-blockcypher-webhooks', 
-    handler: function (request, h) {
-        //logger.info('payload', request.payload);
+amqp
+  .connect(AMQP_URL)
+  .then(async (conn) => {
+    logger.info("amqp:connected", AMQP_URL);
 
-        handleBlockcypherTx(request.payload);
+    conn.on("error", error => {
+      logger.error("amqp:connection:error", error);
+      process.exit(1);
+    });
 
-        return 'success';
-    }
+    conn.on("close", () => {
+      logger.error("amqp:connection:closed");
+      process.exit(1);
+    });
+
+  // Add the route
+  server.route({
+      method: 'POST',
+      path:'/oracles/dash-blockcypher-webhooks', 
+      handler: function (request, h) {
+          //logger.info('payload', request.payload);
+
+          handleBlockcypherTx(request.payload);
+
+          return 'success';
+      }
+  });
+  /*
+
+  server.route({
+      method: 'POST',
+      path:'/oracles/dash-blockcypher-webhooks/{address}', 
+      handler: function (request, h) {
+
+          handleBlockcypherTx(request.payload);
+
+          return 'success';
+      }
+  });
+  */
+
+  // Start the server
+  async function start() {
+
+      try {
+          await server.start();
+      }
+      catch (err) {
+          console.log(err);
+          process.exit(1);
+      }
+
+      console.log('Server running at:', server.info.uri);
+  };
+
+  start();
+
 });
-
-server.route({
-    method: 'POST',
-    path:'/oracles/dash-blockcypher-webhooks/{address}', 
-    handler: function (request, h) {
-
-        handleBlockcypherTx(request.payload);
-
-        return 'success';
-    }
-});
-
-// Start the server
-async function start() {
-
-    try {
-        await server.start();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    console.log('Server running at:', server.info.uri);
-};
-
-start();
 
 module.exports = server;
 
