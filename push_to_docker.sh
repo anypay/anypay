94b5e092363a7fc7103c9a3668e434ec66b4ae48
Steven Zeiler <me@stevezeiler.com>
2018-02-03 14:33:58 -0500

    rename docker tag to payment-service more generic, works with all containers

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -2,5 +2,5 @@ docker login --email $DOCKER_EMAIL \
              --username $DOCKER_USER \
              --password $DOCKER_PASSWORD 
 
-docker tag paymentservice_rest-api anypay/paymentservice_rest-api:latest
+docker tag payment-service anypay/paymentservice_rest-api:latest
 docker push anypay/paymentservice_rest-api:latest
