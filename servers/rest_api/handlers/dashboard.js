c1e11a1aa60fbcb7d1fb34266df60dab022867d4
Derrick J <derrickjfreeman@gmail.com>
2018-02-03 15:46:16 -0500

    updated validate tokens call to fit hapi protocol (#70)
    * updated validate tokens call to fit hapi protocol
    
    * return credentials object in basic auth

--- servers/rest_api/handlers/dashboard.js
+++ servers/rest_api/handlers/dashboard.js
@@ -1,37 +1,26 @@
-
-const sequelize=require('../../../lib/database');
-    const bitcoinPrice=require('../../../lib/bitcoin/price');
-    const bitcoinCashPrice=require('../../../lib/bitcoin_cash/price');
-    const dashPrice=require('../../../lib/dash/price');
-    const dogecoinPrice=require('../../../lib/dogecoin/price');
-    const litecoinPrice=require('../../../lib/litecoin/price');
-    module.exports.index=function(request, reply) {
-    let currentDate=new Date();
-        currentDate.setMonth(currentDate.getMonth()-1);
-        let currentMonth=currentDate.getMonth();
-        let currentYear=currentDate.getFullYear();
-        //let currentDay=currentDate.getDate();
-        let currentDay=01;
-        let targetDate=currentYear+'-'+currentMonth+'-'+currentDay+'T00:00:00.0Z';
-        let accountId=request.auth.credentials.accessToken.account_id;
-        var sql='';
-        if ([41, 14, 125, 177].indexOf(request.auth.credentials.accessToken.account_id)<0){
-    sql=`select*from invoices where "account_id"='`+accountId+`' AND  "createdAt">'`+targetDate+`'`;
-    } else{
-    sql=`select*from invoices where "createdAt">'`+targetDate+`'`;
-    }
-    sequelize.query(sql, {
-    type: sequelize.QueryTypes.SELECT
+const sequelize = require('../../../lib/database');
+module.exports.index = function(request, reply) {
+  let accountId = 14;
+  let currentDate = new Date();
+  let currentYear = currentDate.getFullYear();
+  let currentMonth = currentDate.getMonth() + 1;
+  let currentDay = currentDate.getDate() + 1;
+  let targetDate = currentYear + '-' + currentMonth + '-' + currentDay + 'T00:00:00.0Z';
+  sequelize.query(`select*
+                        from invoices
+                        where "createdAt">'` + targetDate + `'
+                        `, {
+      type: sequelize.QueryTypes.SELECT
     })
-        .then(invoices=>{
-        reply({ invoices: invoices, targetDate:targetDate, prices:{
-        BTC:bitcoinPrice.getDollarPrice(),
-            BCH:bitcoinCashPrice.getDollarPrice(),
-            DASH:dashPrice.getDollarPrice(),
-            DOGE:dogecoinPrice.getDollarPrice(),
-            LTC:litecoinPrice.getDollarPrice()}});
-        })
-        .catch(error=>{
-        reply({ error: error.message }).code(500);
-        });
-    }
+    .then(invoices => {
+      reply({
+        invoices: invoices,
+        targetDate: targetDate
+      });
+    })
+    .catch(error => {
+      reply({
+        error: error.message
+      }).code(500);
+    });
+}
