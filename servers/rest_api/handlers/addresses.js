011932a97998afdfbb58a2172b849da5e3a2d74e
Steven Zeiler <zeiler.steven@gmail.com>
2018-02-05 13:39:14 -0500

    upgrade addresses handler

--- servers/rest_api/handlers/addresses.js
+++ servers/rest_api/handlers/addresses.js
@@ -1,26 +1,21 @@
 const BitcoinInvoice = require("../../../lib/bitcoin/invoice");
 const Account = require("../../../lib/models/account");
+const Boom = require('boom');
 
-module.exports.list = function(request, reply) {
-
+module.exports.list = async function(request, reply) {
 
   let accountId = request.auth.credentials.accessToken.account_id;
 
-  Account.find({ where: { id: accountId }})
-    .then(account => {
+  let account = await Account.find({ where: { id: accountId }})
 
-      reply({
-        'BTC': account.bitcoin_payout_address,
-        'DASH': account.dash_payout_address,
-        'BCH': account.bitcoin_cash_address
-      });
-    })
-    .catch(error => {
-      reply({ error: error.message }).code(500);
-    });
-}
+  return {
+    'BTC': account.bitcoin_payout_address,
+    'DASH': account.dash_payout_address,
+    'BCH': account.bitcoin_cash_address
+  };
+};
 
-module.exports.update = function(request, reply) {
+module.exports.update = async function(request, reply) {
   let currency = request.params.currency;
   let address = request.payload.address;
   let accountId = request.auth.credentials.accessToken.account_id;
@@ -59,16 +54,13 @@ module.exports.update = function(request, reply) {
   }
 
   if (!updateParams) {
-    reply({ error: 'valid currency and address must be provided' }).code(500);
-    return;
+
+    return Boom.badRequest('valid currency and address must be provided');
+
   } else {
-    Account.update(updateParams, {where: { id: accountId }})
-      .then(invoice => {
-        reply(invoice);
-      })
-      .catch(error => {
-        reply({ error: error.message }).code(500);
-      });
+    var invoice = await Account.update(updateParams, {where: { id: accountId }})
+
+    return invoice;
   }
 }
 
