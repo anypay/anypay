caa34358665af33213aecac704e6798d26c692f0
Steven Zeiler <zeiler.steven@gmail.com>
2018-02-05 15:21:32 -0500

    Fix invoices controllers with new hapi :/

--- servers/rest_api/handlers/invoices.js
+++ servers/rest_api/handlers/invoices.js
@@ -1,23 +1,42 @@
 const Invoice = require("../../../lib/models/invoice");
+const log = require('winston');
 
 module.exports.index = async (request, reply) => {
-  var invoice = await Invoice.findAll({
+
+  log.info(`controller:invoices,action:index`);
+
+  var invoices = await Invoice.findAll({
     where: {
       account_id: request.auth.credentials.accessToken.account_id
     }
   });
+
+  return invoices;
 };
 
 module.exports.show = async function(request, reply) {
+
+  let invoiceId = request.params.invoice_id;
+
+  log.info(`controller:invoices,action:show,invoice_id:${invoiceId}`);
+
   let invoice = await Invoice.findOne({
     where: {
-      uid: request.params.invoice_id
+      uid: invoiceId
     }
   });
 
-  if (!invoice) {
+  if (invoice) {
+
+    log.info('invoice found', invoice.toJSON());
+
+    return invoice;
+
+  } else {
+
+    log.error('no invoice found', invoiceId);
+
     throw new Error('invoice not found')
   }
 
-  return invoice;
 }
