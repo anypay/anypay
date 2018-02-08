387531fb7205e3017f5b7694ba4fe963e3419678
Steven Zeiler <zeiler.steven@gmail.com>
2018-02-07 23:30:01 -0500

    Ethereum oracle (#72)
    * Initial ether blockcypher oracle
    
    * use separate ether information for blockcypher
    
    * remove unused ether oracles meta files

---
+++ oracles/ether-blockcypher-webhooks/blockcypher.js
@@ -0,0 +1,45 @@
+const http = require("superagent");
+
+const token = process.env.BLOCKCYPHER_TOKEN;
+const webhookUrl = process.env.BLOCKCYPHER_ETHER_WEBHOOK_URL ||
+  'https://blockcypher.anypay.global/ether-blockcypher-webhooks'
+
+if (!token) {
+  throw new Error('BLOCKCYPHER_TOKEN environment variable required');
+}
+
+module.exports.createWebhook = function createWebhook(address) {
+
+  return new Promise((resolve, reject) => {
+    let webhook = {
+      event: "unconfirmed-tx",
+      address: address,
+      url: webhookUrl
+    };
+
+    http
+      .post(`https://api.blockcypher.com/v1/eth/main/hooks?token=${token}`)
+      .send(webhook)
+      .end((error, response) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(response.body.id);
+      });
+  });
+}
+
+module.exports.deleteWebhook = function deleteWebhook(webhookId) {
+
+  return new Promise((resolve, reject) => {
+    http
+      .del(`https://api.blockcypher.com/v1/eth/main/hooks/${webhookId}?token=${token}`)
+      .end((error, response) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(response.body);
+      });
+  });
+}
+
