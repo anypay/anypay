fbc2f121030bec61276be71515eddfbb9edd621e
Ben Woosley <Ben.Woosley@gmail.com>
2018-05-02 16:58:29 -0400

    Upgrade zcash invoice handler to current hapi api (#92)
    Which does not include "reply" as an argument.

--- servers/rest_api/handlers/zcash_invoices.js
+++ servers/rest_api/handlers/zcash_invoices.js
@@ -1,21 +1,13 @@
 const ZcashInvoice = require("../../../lib/zcash/invoice");
 
-module.exports.create = function(request, reply) {
-  let dollarAmount = request.payload.amount;
-  let accountId = request.auth.credentials.accessToken.account_id;
-  let encrypted = request.payload.encrypted;
+module.exports.create = function(request, h) {
+  const dollarAmount = request.payload.amount;
+  const accountId = request.auth.credentials.accessToken.account_id;
+  const encrypted = request.payload.encrypted;
 
-
-  ZcashInvoice.generate({
+  return ZcashInvoice.generate({
     dollar_amount: dollarAmount,
     account_id: accountId,
     encrypted: encrypted
-  })
-  .then(invoice => {
-    reply(invoice)
-  })
-  .catch(error => {
-    reply({ error: error.message }).code(500);
   });
 }
-
