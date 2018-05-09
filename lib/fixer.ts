bd3ed423f362104a6c699d7c712513a317594719
Steven Zeiler <zeiler.steven@gmail.com>
2018-05-09 15:57:59 -0400

    Service to download and cache foreign currency rates (#97)
    Denominated in BTC

---
+++ lib/fixer.ts
@@ -0,0 +1,39 @@
+
+const http = require('superagent');
+
+const apiKey = process.env.ANYPAY_FIXER_ACCESS_KEY
+
+const url = `http://data.fixer.io/api/latest?access_key=${apiKey}&base=btc`
+
+var cache;
+
+async function updateCurrencies() {
+
+  let response = await http.get(url);
+
+  cache = response.body;
+
+  return;
+}
+
+setInterval(async () => {
+    console.log('update base currencies rates');
+    await updateCurrencies();
+
+}, 1000 * 60 * 60 * 12); // every twelve hours
+
+(async () => {
+
+  await updateCurrencies();
+
+})()
+
+module.exports.getCurrencies = async () => {
+
+  if (!cache) {
+    await updateCurrencies();
+  }
+
+  return cache;
+}
+
