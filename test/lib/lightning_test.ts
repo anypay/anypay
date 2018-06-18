9d700c983b7ab691bf582f88e49eed468ef3a1b4
Steven Zeiler <me@stevenzeiler.com>
2018-06-17 22:58:16 -0400

    lightning library for creating

---
+++ test/lib/lightning_test.ts
@@ -0,0 +1,48 @@
+require('dotenv').config();
+import * as assert from 'assert';
+import {create, generateLightningInvoice} from '../../lib/lightning/invoice';
+import * as Chance from 'chance';
+import {Account} from '../../lib/models';
+
+const chance = new Chance();
+
+describe("Lightning Library", () => {
+
+  describe('Generating Lightning Invoice Without Save', async () => {
+
+    it("#generateLightningInvoice should generate a new invoice", async () => {
+
+      let lightningInvoice = await generateLightningInvoice(10);
+
+      console.log(lightningInvoice);
+
+      return;
+
+    });
+
+  });
+
+  describe('Generating Lightning Invoice With Save', async () => {
+
+    it("#create should genreate and save an invoice", async () => {
+      let account = await Account.create({ email: chance.email() })
+      console.log('account', account);
+      let amount = 10;
+
+      try {
+      let invoiceRecord = await create(amount, account.id);
+
+        console.log('invoice created', invoiceRecord);
+      } catch(error) {
+        console.log('invoice error');
+        console.error(error.message);
+        throw error;
+      }
+
+      return;
+    });
+
+
+  });
+
+});
