f49bd6b3af5cc2f2c2939d5023556de56b2dab61
Steven Zeiler <me@stevezeiler.com>
2018-07-09 20:23:47 -0400

    generate DASH invoice based on account denomination

--- lib/prices/index.ts
+++ lib/prices/index.ts
@@ -1,23 +1,26 @@
+
 import { getLegacyPrices } from './legacy';
 import { getCryptoPrices } from './crypto';
+
+const MAX_DECIMALS = 5;
 
 interface Amount {
   currency: string;
   value: number;
-}
+};
 
 interface Conversion {
   input: Amount;
   output: Amount;
   timestamp: Date
-}
+};
 
 async function getAllPrices() {
   let legacyPrices = await getLegacyPrices();
   let cryptoPrices = await getCryptoPrices();
 
   return Object.assign(legacyPrices, cryptoPrices);
-}
+};
 
 async function createConversion(inputAmount: Amount, outputCurrency: string): Promise<Conversion> {
   let input = inputAmount;
@@ -28,8 +31,8 @@ async function createConversion(inputAmount: Amount, outputCurrency: string): Pr
     input,
     output,
     timestamp
-  }
-}
+  };
+};
 
 async function convert(inputAmount: Amount, outputCurrency: string): Promise<Amount> {
 
@@ -40,10 +43,11 @@ async function convert(inputAmount: Amount, outputCurrency: string): Promise<Amo
 
   return {
     currency: outputCurrency,
-    value: targetAmount
-  }
-}
+    value: parseFloat(targetAmount.toFixed(MAX_DECIMALS))
+  };
+};
 
 export {
   convert, createConversion
-}
+};
+
