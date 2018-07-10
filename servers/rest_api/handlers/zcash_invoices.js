a6d39f8a36d3cf5a1ab2217e645bbbff8e2b8456
Steven Zeiler <me@stevezeiler.com>
2018-07-09 20:59:39 -0400

    make zcash handler async function

--- servers/rest_api/handlers/zcash_invoices.js
+++ servers/rest_api/handlers/zcash_invoices.js
@@ -1,6 +1,6 @@
 import {generateInvoice} from '../../../lib/invoice';
 
-module.exports.create = function(request, h) {
+module.exports.create = async function(request, h) {
   const denominationAmount = request.payload.amount;
   const accountId = request.auth.credentials.accessToken.account_id;
 
