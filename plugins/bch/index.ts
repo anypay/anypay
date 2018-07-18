9b41c24ea52b8cf720232d3e33d19dde9f4297cb
Steven Zeiler <me@stevenzeiler.com>
2018-07-17 20:54:14 -0400

    generate invoice address with q style validate address in the process

--- plugins/bch/index.ts
+++ plugins/bch/index.ts
@@ -1,6 +1,20 @@
+var JSONRPC = require('./lib/jsonrpc');
+
+var rpc = new JSONRPC();
 
 export async function generateInvoiceAddress(settlementAddress: string): Promise<string> {
 
-  return settlementAddress;
+  let response = await rpc.call('validateaddress', [settlementAddress]);
+
+  if (response.result.isvalid) {
+
+    return response.result.address;
+
+  } else {
+
+    throw new Error(`invalid address ${settlementAddress}`);
+
+  }
+
 }
 
