9bd3b093665166b5e9bd82f179077eafb65b54bc
Steven Zeiler <me@stevezeiler.com>
2018-07-23 16:16:29 -0400

    Return global total number of payments in DASH by month

--- lib/totals/monthly.ts
+++ lib/totals/monthly.ts
@@ -25,8 +25,58 @@ function forCurrencyForAccount(accountId: number)  {
   }
 }
 
+function countForCurrencyForAccount(accountId: number)  {
+
+  return async function(currency: string) {
+
+    const query = `select extract(month from "createdAt") as mon,
+      extract(year from "createdAt") as yyyy,
+      sum(1) as "total"
+      from invoices
+      where status = 'paid'
+      and account_id = ${accountId}
+      and currency = '${currency}'
+      group by 1,2;`;
+
+    var result = await database.query(query);
+
+    return result[0].map(res => {
+      return {
+        month: res.mon,
+        year: res.yyyy,
+        total: parseFloat(res.total)
+      }
+    });
+  }
+}
+
+export async function globalTotalsForCurrency(currency: string) {
+
+  const query = `select extract(month from "createdAt") as mon,
+    extract(year from "createdAt") as yyyy,
+    sum(1) as "total"
+    from invoices
+    where status = 'paid'
+    and currency = '${currency}'
+    group by 1,2;`;
+
+  var result = await database.query(query);
+
+  return result[0].map(res => {
+    return {
+      month: res.mon,
+      year: res.yyyy,
+      total: parseFloat(res.total)
+    }
+  });
+}
+
 export function forAccount(accountId: number) {
 
-  return { forCurrency: forCurrencyForAccount(accountId) }
+  return {
+    forCurrency: forCurrencyForAccount(accountId),
+    countForCurrency: countForCurrencyForAccount(accountId)
+  }
 }
+
 
