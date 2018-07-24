f11c3c2fc458beb8f1a35dfdf69c0995292a95b3
Steven Zeiler <me@stevenzeiler.com>
2018-07-24 13:00:15 -0400

    script to notify payroll cron

---
+++ bin/notify_payroll_cron.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env ts-node
+
+require("dotenv").config();
+import * as amqp from 'amqplib';
+
+(async function() {
+
+  var conn = await amqp.connect(process.env.AMQP_URL);
+
+  console.log('amqp.connected');
+
+  let chan = await conn.createChannel();
+
+  console.log('amqp.channel.created');
+
+  await chan.assertExchange('anypay.payroll');
+
+  console.log('amqp.exchange.asserted', 'anypay.payroll');
+
+  await chan.publish('anypay.payroll', 'daily', new Buffer(new Date().toString()));
+
+  console.log('amqp.published');
+
+  setTimeout(() => conn.close(), 5000);
+
+})();
+
