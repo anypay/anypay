e034725f57d59a8f635bf6ece7b454bd385bf752
Steven Zeiler <me@stevezeiler.com>
2018-07-24 21:40:13 -0400

    pushlish message for each payroll account daily

--- bin/notify_payroll_cron.js
+++ bin/notify_payroll_cron.js
@@ -2,6 +2,13 @@
 
 require("dotenv").config();
 var amqp = require('amqplib');
+var database = require('../lib/database');
+
+const payrollQuery = `SELECT
+   accounts.email, accounts.dash_payout_address, payroll_accounts.*
+  FROM payroll_accounts
+  join accounts
+    on accounts.id = payroll_accounts.account_id LIMIT 100;`;
 
 (async function() {
 
@@ -17,9 +24,23 @@ var amqp = require('amqplib');
 
   console.log('amqp.exchange.asserted', 'anypay.payroll');
 
-  await chan.publish('anypay.payroll', 'daily', new Buffer(new Date().toString()));
+  let payroll = (await database.query(payrollQuery))[0];
 
-  console.log('amqp.published');
+  payroll.forEach(async account => {
+
+    const DAYS_PER_MONTH = 30.4167
+
+    account.base_monthly_amount = parseFloat(account.base_monthly_amount);
+
+    account.base_daily_amount = account.base_monthly_amount / DAYS_PER_MONTH;
+
+    let message = new Buffer(JSON.stringify(account));
+
+    await chan.publish('anypay.payroll', 'daily', message);
+
+    console.log('amqp.published', account);
+
+  });
 
   setTimeout(() => conn.close(), 5000);
 
