95ad3f0a7f7d0175c40e9c2b052798335e30a89d
Steven Zeiler <me@stevezeiler.com>
2018-07-26 16:54:08 -0400

    Separate actors to handle each type of BCH event

---
+++ plugins/bch/actors/rawtx_unprocessed.ts
@@ -0,0 +1,39 @@
+
+require("dotenv").config();
+
+import * as amqp from 'amqplib';
+
+import {notifySlack} from '../lib/slack';
+
+const queue = 'anypay.bch.rawtx';
+
+async function start() {
+
+  let connection = await amqp.connect(process.env.AMQP_URL); 
+
+  let channel = await connection.createChannel();
+
+  channel.consume(queue, async (message) => {
+    
+    let content = message.content.toString('hex');
+    
+    console.log(queue, content);
+
+    channel.ack(message);
+
+    await notifySlack(queue, content);
+
+  });
+
+}
+
+export {
+  start
+};
+
+if (require.main === module) {
+
+  start();
+
+}
+
