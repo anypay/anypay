ca4feb5987c1884af5475fa06c191c8051fd63c7
Steven Zeiler <me@stevezeiler.com>
2018-07-30 19:15:03 -0400

    Default dash back totals to 0

--- lib/dashback/index.ts
+++ lib/dashback/index.ts
@@ -7,7 +7,17 @@ export async function getCustomerTotalsAllTime(): Promise<number> {
     `select sum(amount) from dash_back_customer_payments`
   );
 
-  return parseFloat(customerPayments[0][0].sum);
+  let amount = parseFloat(customerPayments[0][0].sum);
+
+  if (amount > 0) {
+
+    return amount;
+
+  } else {
+
+    return 0;
+
+  }
 
 }
 
@@ -17,7 +27,17 @@ export async function getMerchantTotalsAllTime(): Promise<number> {
     `select sum(amount) from dash_back_customer_payments`
   );
 
-  return parseFloat(merchantPayments[0][0].sum);
+  let amount = parseFloat(merchantPayments[0][0].sum);
+
+  if (amount > 0) {
+
+    return amount;
+
+  } else {
+
+    return 0;
+
+  }
 
 }
 
