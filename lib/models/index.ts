62749321d64b09a30a81e0dcf5d68ce8be24e26f
Steven Zeiler <me@stevenzeiler.com>
2018-08-21 02:51:16 -0400

    Merchant Bounty Rewards sequelize model & database table command line interface model test

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -3,6 +3,7 @@ var Address = require('./address');
 var DashBackMerchant = require('./dash_back_merchant');
 var DashBackMerchantPayment = require('./dash_back_merchant_payment');
 var DashBackCustomerPayment = require('./dash_back_customer_payment');
+var MerchantBountyReward = require('./merchant_bounty_reward');
 var Invoice = require('./invoice');
 
 export {
@@ -10,6 +11,7 @@ export {
   DashBackMerchant,
   DashBackMerchantPayment,
   DashBackCustomerPayment,
+  MerchantBountyReward,
   Invoice,
   Address
 };
