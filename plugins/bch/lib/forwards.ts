ce4edc170f8cd9e155ebfe7cf927e614595f406c
stevenzeiler <me@stevenzeiler.com>
2018-10-21 23:32:55 -0400

    Method to forward BCH payments

--- plugins/bch/lib/forwards.ts
+++ plugins/bch/lib/forwards.ts
@@ -7,6 +7,18 @@ var JSONRPC = require('./jsonrpc');
 
 var rpc = new JSONRPC();
 
+interface Payment {
+
+  currency: string;
+
+  amount: number;
+
+  address: string;
+
+  hash: string;
+
+}
+
 export async function setupPaymentForward(outputAddress: string) {
 
   let result = await rpc.call('getnewaddress');
@@ -35,5 +47,49 @@ export async function setupPaymentForward(outputAddress: string) {
 
   return record;
   
+}
+
+export async function forwardPayment(payment: Payment) {
+
+  let paymentForward = await forwards.getPaymentForwardByInput({
+
+    address: payment.address,
+
+    currency: 'BCH'
+
+  });
+
+  let rpcResult = await rpc.call('sendtoaddress', [paymentForward.output_address, payment.amount.toString()]);
+
+  log.info('bch.forwardpayment.result', rpcResult);
+
+  let txid = rpcResult.result;
+
+  let paymentForwardInputPayment = await forwards.createPaymentForwardInputPayment(paymentForward.id, {
+
+    amount: payment.amount,
+
+    txid: payment.hash
+
+  });
+
+  let paymentForwardOutputPayment = await forwards.createPaymentForwardOutputPayment(
+    
+    paymentForward.id,
+    
+    paymentForwardInputPayment.id,
+
+    {
+    
+      amount: payment.amount,
+
+      txid: txid
+
+    }
+
+  );
+
+  return paymentForwardOutputPayment;
+
 }
 
--- plugins/bch/test/payment_forwards_test.ts
+++ plugins/bch/test/payment_forwards_test.ts
@@ -4,6 +4,8 @@ import * as assert from 'assert';
 
 import { forwards } from '../';
 
+import { models } from '../../../lib';
+
 describe('BCH Payment Forwards', () => {
 
   it('setupPaymentForward should generate an address and save a payment_forward record', async () => {
@@ -17,6 +19,46 @@ describe('BCH Payment Forwards', () => {
 
     assert.strictEqual(paymentForward.output_currency, 'BCH');
     assert.strictEqual(paymentForward.output_address, outputAddress);
+
+  });
+
+  it('#forwardPayment should send payment and create input and output records', async () => {
+
+    let outputAddress = 'bitcoincash:qrup980ejnvjk8v8vawt5w59nc8nk08d6gc0nvylht';
+
+    let paymentForward = await forwards.setupPaymentForward(outputAddress);
+
+    let txid = '0589778c2e95b58b9ae2d8dcc616d6bf8dd587646d933450f4f878f3cb9b6f69'; // note does not match forwarding address
+
+    let payment = {
+
+      currency: 'BCH',
+
+      amount: 0.001,
+
+      address: paymentForward.input_address,
+
+      hash: txid
+
+    };
+
+    let paymentForwardOutputPayment = await forwards.forwardPayment(payment);
+
+    assert.strictEqual(paymentForwardOutputPayment.payment_forward_id, paymentForward.id);
+
+    assert.strictEqual(parseFloat(paymentForwardOutputPayment.amount), payment.amount);
+
+    let paymentForwardInputPayment = await models.PaymentForwardInputPayment.findOne({ where: {
+
+      id: paymentForwardOutputPayment.payment_forward_input_payment_id
+
+    }});
+
+    assert.strictEqual(paymentForwardInputPayment.payment_forward_id, paymentForward.id);
+
+    assert(paymentForwardInputPayment.txid);
+
+    assert.strictEqual(parseFloat(paymentForwardInputPayment.amount), payment.amount);
 
   });
 
