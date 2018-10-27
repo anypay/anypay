276235b5ec8347429389924afabf7d51a7ffb272
Steven Zeiler <zeiler.steven@gmail.com>
2018-10-27 16:44:29 -0400

    Ves price localbitcoins (#182)
    * get VES price from localbitcoins.com
    
    * Cache the VES price every ten minutes

--- bin/prices.ts
+++ bin/prices.ts
@@ -5,15 +5,54 @@ require('dotenv').config();
 const program = require('commander')
 
 import { getPriceOfOneDollarInVES } from '../lib/prices/ves';
+import { getVESPrice } from '../lib/localbitcoins';
 import { getAllPrices } from '../lib/prices';
+import { createConversion } from '../lib/prices';
+
+program
+  .command('convert <baseamount> <basecurrency> <targetcurrency>')
+  .action(async (value, currency, targetCurrency) => {
+
+    try {
+
+      let conversion = await createConversion({
+
+        value, currency 
+
+      }, targetCurrency);
+
+      console.log(conversion);
+
+    } catch(error) {
+
+      console.error(error.message);
+
+    }
+
+  });
 
 program
   .command('getvesprice')
   .action(async () => {
 
-    let price = await getPriceOfOneDollarInVES();
+    let usdPrice = await getPriceOfOneDollarInVES();
 
-    console.log(`one dollar equals ${price}`);
+    let allPrices = await getAllPrices();
+
+
+    let btcPrice = usdPrice * allPrices['USD'];
+
+    console.log(`one BTC equals ${btcPrice} VES`);
+
+  });
+
+program
+  .command('getvespricelocalbitcoins')
+  .action(async () => {
+
+    let price = await getVESPrice();
+
+    console.log(`one BTC equals ${price} VES on localbitcoins.com`);
 
   });
 
