866f7f990a77334af7e6f33b9372bcbbae1193d2
Steven Zeiler <me@stevezeiler.com>
2018-11-02 14:49:03 -0400

    Complete payroll automation command line tool `./bin/payroll.ts pay satoshi@anypay.global BCH`
    Could still be refactored into a library to reuse
    in a web application or cron job.

--- bin/payroll.ts
+++ bin/payroll.ts
@@ -8,7 +8,21 @@ import { models, log, prices } from '../lib';
 
 import { getSupportedCoins } from '../lib/accounts';
 
+import { sendPayrollReceipt } from '../lib/payroll/email';
+
 import * as bch from '../plugins/bch';
+
+program
+  .command('listaccounts')
+  .action(async () => {
+
+    let payrollAccounts = await models.PayrollAccount.findAll();
+
+    payrollAccounts.map((account) => log.info(account.toJSON()));
+
+    process.exit(0);
+
+  });
 
 program
   .command('addaccount <email> <base_monthly_amount> <base_currency>')
@@ -54,6 +68,14 @@ program
 
     let account = await models.Account.findOne({ where: { email }});
 
+    if (!account) {
+
+      log.error(`no account found with email ${email}`);
+
+      process.exit(0);
+
+    }
+
     let payrollAccount = await models.PayrollAccount.findOne({ where: {
 
       account_id: account.id
@@ -81,13 +103,65 @@ program
     log.info('rpc result', result);
 
     let payrollPayment = await models.PayrollPayment.create({
-      payroll_account_id: payrollAccount.id
-      amount: amount,
-      address: address,
-      hash: result.result
+      payroll_account_id: payrollAccount.id,
+      amount,
+      address,
+      currency,
+      hash: result.result,
     });
 
+    log.info('payroll payment', payrollPayment.toJSON());
+
+    let emailResult = await sendPayrollReceipt(payrollPayment.id);
+
+    log.info('payroll email receipt sent!')
+
   });
+
+program
+  .command('sendemailreceipt <email>')
+  .action(async (email) => {
+
+    let account = await models.Account.findOne({ where: { email  }});
+
+    if (!account) {
+
+      log.error(`no account found with email ${email}`);
+
+      process.exit(0);
+
+    }
+
+    let payrollAccount = await models.PayrollAccount.findOne({ where: {
+
+      account_id: account.id
+
+    }});
+
+    let latestPayrollPayment = await models.PayrollPayment.findOne({
+
+      where: { payroll_account_id: payrollAccount.id }
+
+    });
+
+    log.info('latestpayrollpayment', latestPayrollPayment.toJSON());
+
+    try {
+
+      let emailResult = await sendPayrollReceipt(latestPayrollPayment.id);
+      
+      console.log(`email sent ${emailResult}`);
+
+    } catch(error) {
+
+      log.error(error.message);
+
+    }
+
+    process.exit(0);
+
+  });
+
 
 program.parse(process.argv);
 
