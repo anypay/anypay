6fc058fda17ecec4fcd3265f02253f1ed6942f8b
brandonbryant12 <brandonbryant002@gmail.com>
2018-11-06 13:33:38 -0500

    destination tag created is set to 8 digits not 10

--- plugins/xrp/lib/ripple_restAPI.ts
+++ plugins/xrp/lib/ripple_restAPI.ts
@@ -4,6 +4,8 @@ const http = require("superagent");
 
 import { connection, channel } from './amqp';
 
+const WebSocket = require('ws');
+
 const routing_key = 'payment';
 
 const exchange = 'anypay.payments';
@@ -59,5 +61,88 @@ export async function ripple_restAPI_checkAddressForPayments(address:string){
     resolve(payments)
 
   })
+
+}
+
+export async function rippleLib_checkAddressForPayments(address:string){
+
+  let payments: Payment[]=[];
+
+  let req = {
+
+      "id": 2,
+      "command": "account_tx",
+      "account": address,
+      "ledger_index_min": -1,
+      "ledger_index_max": -1,
+      "binary": false,
+      "limit": 10,
+      "forward": false
+  }
+ 
+  let ws = new WebSocket('wss://s2.ripple.com:443')
+  
+  ws.on('open',()=>{
+
+     ws.send(JSON.stringify(req));
+
+  })
+
+  ws.on('message',(data)=>{
+
+    let res = JSON.parse(data);
+
+    for( let i = 0; i<res.result.transactions.length;i++){
+
+      if( res.result.transactions[i].tx.TransactionType != 'Payment'){continue}
+
+      let currency = "XRP"
+
+      let amount = 0
+
+      if( typeof(res.result.transactions[i].tx.Amount) == 'string'){
+
+        amount = res.result.transactions[i].tx.Amount
+
+      }
+
+      else{
+
+        currency = "XRP."+res.result.transactions[i].tx.Amount.currency+"."+res.result.transactions[i].tx.Amount.issuer
+
+        amount = res.result.transactions[i].tx.Amount.value
+
+      }
+
+      let receiveAddress = res.result.transactions[i].tx.Destination
+
+      if( typeof(res.result.transactions[i].tx.DestinationTag) != 'undefined'){ 
+      
+        receiveAddress = res.result.transactions[i].tx.Destination+'?'+res.result.transactions[i].tx.DestinationTag
+     
+      }
+      
+      let p: Payment = {
+
+        currency: currency,
+        address: receiveAddress,
+        amount: amount,
+        hash: res.result.transactions[i].tx.hash
+
+      }
+
+      payments.push(p)
+
+      channel.publish(exchange, routing_key, new Buffer(JSON.stringify(p)));
+
+    }
+
+  })
+
+
+  ws.on('close',()=>{console.log})
+
+
+  return payments;
 
 }
