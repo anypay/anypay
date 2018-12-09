fff45954566943fb51d83e0eae2249f646000c45
Steven Zeiler <me@stevenzeiler.com>
2018-12-09 14:00:16 -0500

    actual migration file instead of empty

--- migrations/20181209183330-add-currency-to-extended-public-keys.js
+++ migrations/20181209183330-add-currency-to-extended-public-keys.js
@@ -2,22 +2,20 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.addColumn('extended_public_keys', 'currency', {
+
+      type: Sequelize.STRING,
+
+      allowNull: false
+
+    });
+
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.removeColumn('extended_public_keys', 'currency');
+
   }
 };
