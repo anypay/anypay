26acc5c76189d408ddf9d2809145dd1ba839cff3
brandonbryant12 <brandon.bryant002@gmail.com>
2018-12-13 12:30:01 -0500

    More automated emails (#285)
    * Test for automated emails events, created events for first invoice and first payment, moved the emitter for setAddress out of handler and into lib
    
    * formatted emails with html, added more templates
    
    * touch up formatting on templates
    
    * adding link to merchant map in templates.  Emitting event when an account is created in the handler
    
    * fixing html formatting

---
+++ test/lib/email_invoice_paid.ts
@@ -0,0 +1,69 @@
+import * as assert from 'assert'
+import * as lib from '../../lib';
+import {setAddress} from '../../lib/core';
+import {generateInvoice} from '../../lib/invoice';
+import {handlePayment} from '../../lib/payment_processor';
+import * as Chance from 'chance';
+const chance = new Chance();
+import {emitter} from '../../lib/events'
+
+describe("Emails when invoice is paid", ()=>{
+
+  var account,invoice1, invoice2;
+
+  before(async()=>{
+
+    account = await lib.accounts.create(chance.email(), chance.word())
+
+    invoice1 = await generateInvoice(account.id, 10, 'XRP');
+
+    invoice2 = await generateInvoice(account.id, 1, 'XRP');
+
+  })
+
+  it("should emit an event when the first invoice is paid", (done)=>{
+
+    let sem = false 
+
+    emitter.on('invoice.paid.first', (p)=>{
+      if(!sem){
+        done()
+      }
+      assert(p)
+      sem = true
+    })
+
+    let payment = {
+      currency: "XRP",
+      amount: invoice1.invoice_amount,
+      address: invoice1.address,
+      hash: 'f2fbd4fbb4bb84a22b9be5734473e90e7c8b7465ed8f49e983e73346681635a8'
+    };
+    handlePayment(invoice1, payment);
+
+
+  })
+
+  it.skip("should emit an event when an invoice is paid", (done)=>{
+
+    let sem = false 
+
+    emitter.on('invoice.paid', (p)=>{
+      if(!sem){
+	done()
+      }
+      assert(p)
+      sem = true
+    })
+
+    let payment = {
+      currency: "XRP",
+      amount: invoice2.invoice_amount,
+      address: invoice2.address,
+      hash: 'f2fbd4fbb4bb84a22b9be5734473e90e7c8b7465ed8f49e983e73346681635a8'
+    };
+    handlePayment(invoice2, payment);
+
+  });
+
+});
