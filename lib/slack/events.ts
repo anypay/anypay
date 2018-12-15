935bfa059ebc1c375870c1f70bc63f6503fad63e
Steven Zeiler <me@stevenzeiler.com>
2018-12-14 22:59:13 -0500

    publish to slack only in production

--- lib/slack/events.ts
+++ lib/slack/events.ts
@@ -5,7 +5,7 @@ const SLACK_URL = 'https://hooks.slack.com/services/T7NS5H415/BEW4DEZ5L/TSNBNHbN
 
 export async function publishEventToSlack(message: string) {
 
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV === 'production') {
 
     await http
       .post(SLACK_URL)
