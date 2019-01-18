a9ace024822cd00e32b44694eacabcfd69e8401d
Steven Zeiler <me@stevenzeiler.com>
2019-01-17 22:21:17 -0500

    change payload type to text

--- migrations/20190118021053-create-blockcypher-event.js
+++ migrations/20190118021053-create-blockcypher-event.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       payload: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
