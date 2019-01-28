8a4014061c349764530e0c1048b159cefb61f821
Steven Zeiler <me@stevenzeiler.com>
2019-01-28 12:35:01 -0300

    Parse payment from BCH forwarding callback payload, publish to amqp

--- servers/rest_api/handlers/bch_address_forward_callbacks.ts
+++ servers/rest_api/handlers/bch_address_forward_callbacks.ts
@@ -1,11 +1,27 @@
 
 import { log } from '../../../lib';
 
+import { channel } from '../../../lib/amqp';
+
 export async function create(req, h) {
 
   log.info('bch.addressforwardcallback', req.payload);
 
-  return req.payload;
+  let payment = {
+    currency: "BCH",
+    amount: req.payload.value,
+    address: req.paylod.input_address,
+    hash: req.payload.input_transaction_hash,
+    output_hash: req.payload.destination_transaction_hash
+  }
+
+  log.info('bch.payment', payment);
+
+  let buffer = new Buffer(JSON.stringify(payment));
+
+  await channel.publish('anypay.payments', 'payment', buffer);
+
+  return payment;
 
 }
 
