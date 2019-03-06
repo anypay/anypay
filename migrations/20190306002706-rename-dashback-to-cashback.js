b81f46e2729ff99471440c6c41f5a2e90ba103ac
Steven Zeiler <me@stevenzeiler.com>
2019-03-05 21:37:00 -0300

    Convert dashback to cashback Including models and database table include currency in model, db

---
+++ migrations/20190306002706-rename-dashback-to-cashback.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+
+    await queryInterface.renameColumn('dash_back_merchants', 'cashback_merchants');
+
+    await queryInterface.renameTable('dash_back_customer_payments', 'cashback_customer_payments');
+
+    await queryInterface.renameTable('dash_back_merchant_payments', 'cashback_merchant_payments');
+
+    await queryInterface.addColumn('cashback_customer_payments', 'currency', Sequelize.STRING);
+
+    await queryInterface.addColumn('cashback_merchant_payments', 'currency', Sequelize.STRING);
+
+  },
+
+  down: async function (queryInterface, Sequelize) {
+
+    await queryInterface.renameColumn('cashback_merchants', 'dash_back_merchants');
+
+    await queryInterface.renameColumn('cashback_customer_payments', 'dash_back_customer_payments');
+
+    await queryInterface.renameColumn('cashback_merchant_payments', 'dash_back_merchant_payments');
+
+    await queryInterface.removeColumn('dash_back_customer_payments', 'currency');
+
+    await queryInterface.removeColumn('dash_back_merchant_payments', 'currency');
+
+  }
+};
