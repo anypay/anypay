df2712c78d816a4e682a1b9c4a6bc60ac4e2e7e9
Steven Zeiler <me@stevenzeiler.com>
2019-03-10 11:45:26 -0300

    rename dash-back-to-cashback

---
+++ migrations/20190310144046-renmae-dash-back-to-cashback-merchant-id.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.renameColumn('cashback_customer_payments', 'dash_back_merchant_id', 'cashback_merchant_id')
+    return queryInterface.renameColumn('cashback_merchnat_payments', 'dash_back_merchant_id', 'cashback_merchant_id')
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.renameColumn('cashback_customer_payments', 'cashback_merchant_id', 'dash_back_merchant_id')
+    return queryInterface.renameColumn('cashback_merchant_payments', 'cashback_merchant_id', 'dash_back_merchant_id')
+  }
+};
