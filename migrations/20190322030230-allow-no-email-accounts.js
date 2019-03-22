baec4953cf2f93072e03292567ac921cb36b66b2
Steven Zeiler <me@stevezeiler.com>
2019-03-21 23:23:12 -0400

    allow for creation of anonymous accounts

---
+++ migrations/20190322030230-allow-no-email-accounts.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.sequelize.query('alter table accounts alter column email drop not null;')
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.changeColumn('accounts', 'email', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+  }
+};
