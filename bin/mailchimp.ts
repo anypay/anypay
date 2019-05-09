3bdf2dae4fa69a0df8cf421ca493556ba6428589
Steven Zeiler <me@stevezeiler.com>
2019-05-09 14:07:59 -0400

    command line tool to subscribe all existing accounts to email list in mailchimp

--- bin/mailchimp.ts
+++ bin/mailchimp.ts
@@ -1,12 +1,32 @@
 #!/usr/bin/env ts-node
 
+require('dotenv').config();
+
 import * as program from 'commander';
 
 import * as http from 'superagent';
 
-require('dotenv').config();
+import { models, log } from '../lib';
 
 var Mailchimp = require('mailchimp-api-v3')
+
+async function subscribe(email) {
+
+  let apiKey = process.env.MAILCHIMP_API_KEY;
+
+  var mailchimp = new Mailchimp(process.env.MAILCHIMP_API_KEY);
+
+  let resp = await mailchimp.post({
+    path: '/lists/7bd4603ab9/members',
+    body: {
+      email_address: email,
+      status: 'subscribed'
+    }
+  });
+
+  return resp;
+
+}
 
 program
   .command('register <email>')
@@ -47,6 +67,37 @@ program
     });
 
     console.log(resp);
+
+  });
+
+program
+  .command('subscribeall')
+  .action(async () => {
+
+    let apiKey = process.env.MAILCHIMP_API_KEY;
+
+    var mailchimp = new Mailchimp(apiKey);
+
+    let accounts = await models.Account.findAll();
+
+    for (let i=0; i < accounts.length; i++) {
+
+      let account = accounts[i];
+
+      try {
+
+        await subscribe(account.email);
+
+        log.info(`mailchimp.subscribed`, account.email);
+
+      } catch(error) {
+
+        log.error(error.message);
+
+      }
+
+    }
+
 
   });
 
