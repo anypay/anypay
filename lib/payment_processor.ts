b61d4dde15265dcdd9badc5edf41e7b9bcd40b8f
brandonbryant12 <brandonbryant002@gmail.com>
2019-05-10 14:19:27 -0400

    passing compiler

--- lib/payment_processor.ts
+++ lib/payment_processor.ts
@@ -26,9 +26,9 @@ export async function updateOutput(payment: Payment){
 
      var result = await models.Invoice.update({
        output_hash: payment.output_hash,
-       output_currency: payment.output_currency;
-       output_amount: payment.output_amount;
-       output_address: payment.output_address;
+       output_currency: payment.output_currency,
+       output_amount: payment.output_amount,
+       output_address: payment.output_address,
        completed_at: new Date()
       },
       {
