4eba46af0c2dd12b4c0fa833e70290c37114d337
Steven Zeiler <me@stevezeiler.com>
2019-06-06 11:37:54 -0400

    tag with new image name

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -2,5 +2,7 @@ docker login --username $DOCKER_USER \
              --password $DOCKER_PASSWORD 
 
 docker tag anypay anypay/api.anypay.global:latest
+docker tag anypay anypay/anypay:latest
 
 docker push anypay/api.anypay.global:latest
+docker push anypay/anypay:latest
