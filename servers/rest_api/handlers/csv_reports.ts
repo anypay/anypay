c8c1fa515938dc9db2f1ae9ba187c1a118be513c
Steven Zeiler <me@stevenzeiler.com>
2019-07-02 13:07:33 -0400

    require access token to be passed as query param (#426)
    * require access token to be passed as query param
    
    * remove console logs and commented out code

--- servers/rest_api/handlers/csv_reports.ts
+++ servers/rest_api/handlers/csv_reports.ts
@@ -1,8 +1,10 @@
 
 import * as moment from 'moment';
-import * as Joi from 'joi'
+import * as Joi from 'joi';
+import * as Boom from 'boom';
 
 import { buildReportCsvFromDates } from '../../../lib/wire';
+import * as models from '../../../lib/models';
 
 export async function accountCSVReports(server) {
 
@@ -11,26 +13,30 @@ export async function accountCSVReports(server) {
     path: '/csv_reports',
     handler: async (req, h) => {
 
-      console.log(req.query);
-      console.log(req.account);
+      let token = await models.AccessToken.findOne({ where: {
+
+        uid: req.query.token
+
+      }});
+
+      if (!token) {
+
+        return Boom.unauthorized('invalid access token');
+      }
 
       let start_date = moment(req.query.start_date);
       let end_date = moment(req.query.end_date);
 
       let content = await buildReportCsvFromDates(
-        req.account.id,
+        token.account_id,
         start_date,
         end_date
       );
-
-      console.log(content);
 
       let start_formatted = start_date.format('MM-DD-YYYY');
       let end_formatted = end_date.format('MM-DD-YYYY');
 
       let filename = `anypay_report_${start_formatted}_${end_formatted}.csv`
-
-      console.log(filename);
 
       let response = h.response(content).header("Content-Disposition", `attachment;filename=${filename}`);
 
@@ -39,11 +45,11 @@ export async function accountCSVReports(server) {
     },
     config: {
       tags: ['api'],
-      auth: "token",
       validate: {
         query: {
           start_date: Joi.date().required(),
-          end_date: Joi.date().required()
+          end_date: Joi.date().required(),
+          token: Joi.string().required()
         }
       }
     }
