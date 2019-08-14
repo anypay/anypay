47b98482696898392b09496d24efb2d86e061230
brandonbryant12 <brandonbryant002@gmail.com>
2019-08-14 15:39:28 -0400

    moving the bitcore-lib-dash dependacny into the project because a confilct with bitcore-lib, removing all references to node_modules and replacing with reference to the bitcore-lib-dash directory

--- lib/xpub/index.ts
+++ lib/xpub/index.ts
@@ -1,5 +1,5 @@
 import * as bitcoincash from 'bitcore-lib-cash';
-import * as dash from 'bitcore-lib-dash';
+import * as dash from '../../bitcore-lib-dash';
 
 function getBitcore(currency: string) {
 
