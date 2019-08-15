357b79d6aeac8ce258a846fa9d896b2cc9f6aed3
brandonbryant12 <brandonbryant002@gmail.com>
2019-08-15 13:01:13 -0400

    implement getNewAddress for BTC using xpub key

--- plugins/btc/index.ts
+++ plugins/btc/index.ts
@@ -1,3 +1,4 @@
+require('dotenv').config()
 import {generateInvoice} from '../../lib/invoice';
 
 import * as chainSoAPI from '../../lib/chainSoAPI';
@@ -6,6 +7,12 @@ import {Invoice} from '../../types/interfaces';
 
 import {statsd} from '../../lib/stats/statsd' 
 
+import {HDKeyAddresses} from '../../lib/models';
+
+import {rpc} from './jsonrpc';
+
+const btc = require('bitcore-lib')
+
 var WAValidator = require('anypay-wallet-address-validator');
 
 export function validateAddress(address: string){
@@ -13,6 +20,33 @@ export function validateAddress(address: string){
   let valid = WAValidator.validate( address, 'BTC')
 
   return valid;
+
+}
+
+export async function getNewAddress(deprecatedParam){
+
+  //Create a new HDKeyAddress 
+  let record = await HDKeyAddresses.create({
+
+    currency:'BTC',
+
+    xpub_key:process.env.BTC_XPUB_KEY
+
+  })
+
+  record.address = deriveAddress(process.env.BTC_XPUB_KEY, record.id)
+
+  await record.save()
+
+  await rpc.call('importaddress', [record.address, "", false, false])
+
+  return record.address;
+
+}
+
+function deriveAddress(xkey, nonce){
+
+  return btc.HDPublicKey(xkey).derive(nonce).publicKey.toAddress().toString()
 
 }
 
