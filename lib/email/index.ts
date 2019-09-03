f4f184a14171e8d05187bd51def4deaf4b62a78a
brandonbryant12 <brandonbryant002@gmail.com>
2019-09-03 14:44:40 -0400

    requiring createtemplate

--- lib/email/index.ts
+++ lib/email/index.ts
@@ -7,7 +7,7 @@ import * as database from '../database';
 const log = require("winston");
 const moment = require('moment');
 AWS.config.update({ region: "us-east-1" });
-require('./createTemplate')
+require('./createtemplate')
 
 const FROM_EMAIL = 'Derrick from Anypay <support@anypay.global>';
 
