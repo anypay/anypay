a6595e76fc8de683060c08cf5a06b0c72a9e7ad3
Steven Zeiler <me@stevezeiler.com>
2019-09-04 22:11:08 -0400

    convert payment protocol signature to hex from b64

--- servers/rest_api/handlers/payment_request.ts
+++ servers/rest_api/handlers/payment_request.ts
@@ -1,6 +1,6 @@
 import {generatePaymentRequest} from '../../../plugins/bch/lib/paymentRequest';
 
-const bch = require('bsv'); 
+const bitcoin = require('bsv'); 
 const Message = require('bsv/message'); 
 
 export async function show(req, h){
@@ -9,9 +9,9 @@ export async function show(req, h){
 
   let content = await generatePaymentRequest(params.uid)
 
-  let digest = bch.crypto.Hash.sha256(Buffer.from(JSON.stringify(content))).toString('hex'); 
+  let digest = bitcoin.crypto.Hash.sha256(Buffer.from(JSON.stringify(content))).toString('hex'); 
 
-  var privateKey = bch.PrivateKey.fromWIF(process.env.JSON_PROTOCOL_IDENTITY_WIF);
+  var privateKey = bitcoin.PrivateKey.fromWIF(process.env.JSON_PROTOCOL_IDENTITY_WIF);
 
   var signature = Message(digest).sign(privateKey); 
 
@@ -21,7 +21,7 @@ export async function show(req, h){
 
   response.header('x-signature-type', 'ecc');
   response.header('x-identity',process.env.JSON_PROTOCOL_IDENTITY_ADDRESS );
-  response.header('signature', signature );
+  response.header('signature', Buffer.from(signature, 'base64').toString('hex'));
   response.header('digest', `SHA-256=${digest}`);
 
   return response
