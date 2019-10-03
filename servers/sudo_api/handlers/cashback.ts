4640c98390429320e345ed4eb8cb66b0b8a2cb29
Steven Zeiler <steven@anypayinc.com>
2019-10-03 13:35:09 -0400

    list cashback payments including invoices in sudo (#458)

--- servers/sudo_api/handlers/cashback.ts
+++ servers/sudo_api/handlers/cashback.ts
@@ -1,8 +1,11 @@
 
 import * as dash from '../../../plugins/dash/lib/jsonrpc';
+
 import * as bch from '../../../plugins/bch/lib/jsonrpc';
 
-import { log, models, database } from '../../../lib';
+import { log, models, database, amqp } from '../../../lib';
+
+import * as Boom from 'boom';
 
 import { RPCSimpleWallet } from 'rpc-simple-wallet';
 
@@ -21,6 +24,115 @@ const coins: any = {
   }
 
 };
+
+export async function index(req, h) {
+
+  try {
+
+    let cashbackRecords = await models.CashbackCustomerPayment.findAll({
+      order: [
+        ['createdAt', 'DESC'],
+      ],
+      limit: req.query.limit || 100,
+      include: [
+         { model: models.Invoice, where: { status: 'paid' }}
+      ]
+    });
+
+    return cashbackRecords;
+
+  } catch(error) {
+
+    return Boom.badRequest(error.message);
+
+  }
+
+}
+
+export async function retry(req, h) {
+
+  try {
+
+    let invoice = await models.Invoice.findOne({ where: {
+      uid: req.params.invoice_uid
+    }});
+
+    if (!invoice) {
+      throw new Error(`invoice ${req.params.invoice_uid} not found`);
+    }
+
+    let channel = await amqp.awaitChannel();
+
+    let message = Buffer.from(invoice.uid);
+
+    await channel.sendToQueue('cryptozone:cashback:customers', message);
+
+    return {
+      invoice_uid: invoice.uid,
+      success: true
+    }
+
+  } catch(error) {
+
+    return Boom.badRequest(error.message);
+
+  }
+
+}
+
+export async function dashboard(req, h) {
+
+  try {
+
+    let merchants = await models.CashbackMerchant.findAll();
+
+    let dashPayments = await models.CashbackCustomerPayment.findAll({
+      where: {
+        currency: 'DASH'
+      }
+    });
+
+    let bchPayments = await models.CashbackCustomerPayment.findAll({
+      where: {
+        currency: 'BCH'
+      }
+    });
+
+    let totalBchPaid = await database.query(`select sum(amount) from cashback_customer_payments where currency='BCH'`);
+    let totalDashPaid = await database.query(`select sum(amount) from cashback_customer_payments where currency='DASH'`);
+
+    let bchBalance = await coins['BCH'].wallet.getAddressUnspentBalance();
+    let dashBalance = await coins['DASH'].wallet.getAddressUnspentBalance();
+
+    return [{                                                                       
+      currency: 'BCH',
+      name: 'Bitcon Cash',
+      donation_address: coins['BCH'].address,
+      donations: "?",
+      total_donations: "?",
+      amount_remaining: bchBalance || 0,
+      total_paid: totalBchPaid[0][0].sum,
+      number_payments: bchPayments.length,
+      eligible_merchants: merchants.length,
+    }, {                                                                            
+      currency: 'DASH',
+      name: 'Dash',
+      donation_address: coins['DASH'].address,
+      donations: "?",
+      total_donations: "?",
+      amount_remaining: dashBalance || 0,
+      total_paid: totalDashPaid[0][0].sum,
+      number_payments: dashPayments.length,
+      eligible_merchants: merchants.length
+    }] 
+
+  } catch(error) {
+
+    return Boom.badRequest(error.message);
+
+  }
+
+}
 
 async function updateStats() {
 
@@ -55,51 +167,4 @@ async function updateStats() {
   updateStats();
 
 })();
-
-
-export async function dashboard(req, h) {
-
-  let merchants = await models.CashbackMerchant.findAll();
-
-  let dashPayments = await models.CashbackCustomerPayment.findAll({
-    where: {
-      currency: 'DASH'
-    }
-  });
-
-  let bchPayments = await models.CashbackCustomerPayment.findAll({
-    where: {
-      currency: 'BCH'
-    }
-  });
-
-  let totalBchPaid = await database.query(`select sum(amount) from cashback_customer_payments where currency='BCH'`);
-  let totalDashPaid = await database.query(`select sum(amount) from cashback_customer_payments where currency='DASH'`);
-
-  let bchBalance = await coins['BCH'].wallet.getAddressUnspentBalance();
-  let dashBalance = await coins['DASH'].wallet.getAddressUnspentBalance();
-
-  return [{                                                                       
-    currency: 'BCH',
-    name: 'Bitcon Cash',
-    donation_address: coins['BCH'].address,
-    donations: "?",
-    total_donations: "?",
-    amount_remaining: bchBalance || 0,
-    total_paid: totalBchPaid[0][0].sum,
-    number_payments: bchPayments.length,
-    eligible_merchants: merchants.length,
-  }, {                                                                            
-    currency: 'DASH',
-    name: 'Dash',
-    donation_address: coins['DASH'].address,
-    donations: "?",
-    total_donations: "?",
-    amount_remaining: dashBalance || 0,
-    total_paid: totalDashPaid[0][0].sum,
-    number_payments: dashPayments.length,
-    eligible_merchants: merchants.length
-  }] 
-
-}
 
