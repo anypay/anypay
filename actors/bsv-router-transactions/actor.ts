ba0d83b619b41fa27c6367c22069b917897a915c
brandonbryant12 <brandonbryant002@gmail.com>
2019-10-03 14:49:32 -0400

    actor to watch bsv router transaction and write info to the database

---
+++ actors/bsv-router-transactions/actor.ts
@@ -0,0 +1,67 @@
+/* implements rabbi actor protocol */
+
+require('dotenv').config();
+
+import { Actor, Joi } from 'rabbi';
+
+import {models} from '../../lib';
+
+export async function start() {
+
+  Actor.create({
+
+    exchange: 'anypay.router',
+
+    routingkey: 'bsv.forwarded',
+
+    queue: 'write.output',
+
+    schema: Joi.object({
+
+      input_address: Joi.string(),
+
+      input_hash: Joi.string(),
+
+      output_hash: Joi.string(),
+
+      output_address: Joi.string(),
+
+      output_currency: Joi.string()
+
+    })
+
+  })
+  .start(async (channel, msg, json) => {
+
+    console.log(json)
+
+    let invoice = await models.Invoice.findOne({
+ 
+      where: { address: json.input_address }
+
+    })
+
+    invoice.hash = json.input_hash;
+
+    invoice.output_hash = json.output_hash;
+
+    invoice.output_currency = json.output_currency;
+
+    invoice.output_address = json.output_address;
+
+    await invoice.save()
+
+    console.log(invoice)
+
+    channel.ack(msg);
+
+  });
+
+
+}
+
+if (require.main === module) {
+
+  start();
+
+}
