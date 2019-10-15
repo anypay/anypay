35a884d0754cc911c0d6a71cc4fc0a90cf6eef2b
Steven Zeiler <me@stevezeiler.com>
2019-10-15 11:35:28 -0400

    correctly route paid messages

--- actors/republish-invoice-paid-to-account-id/actor.ts
+++ actors/republish-invoice-paid-to-account-id/actor.ts
@@ -4,7 +4,34 @@ require('dotenv').config();
 
 import { Actor, Joi, log } from 'rabbi';
 
+import { models } from '../../lib';
+
 export async function start() {
+
+  Actor.create({
+
+    exchange: 'anypay:invoices',
+
+    routingkey: 'invoice:paid',
+
+    queue: 'republish_invoice_paid_as_account_id'
+
+  })
+  .start(async (channel, msg) => {
+
+    const uid = msg.content.toString();
+
+    let invoice = await models.Invoice.findOne({ where: { uid }});
+
+    let routingKey = `accounts.${invoice.account_id}.invoicepaid`;
+
+    await channel.publish('anypay.events', routingKey, msg.content);
+
+    log.info(routingKey, { invoice: invoice.toJSON() });
+
+    channel.ack(msg);
+
+  });
 
   Actor.create({
 
@@ -12,12 +39,33 @@ export async function start() {
 
     routingkey: 'models.Invoice.afterCreate',
 
-    queue: 'republish_invoice_paid_as_invoice_id'
+    queue: 'republish_invoice_created_as_account_id'
 
   })
   .start(async (channel, msg, json) => {
 
-    let routingKey = `accounts.${json.account_id}.invoicepaid`;
+    let routingKey = `accounts.${json.account_id}.invoicecreated`;
+
+    await channel.publish('anypay.events', routingKey, msg.content);
+
+    log.info(routingKey, { invoice: json });
+
+    channel.ack(msg);
+
+  });
+
+  Actor.create({
+
+    exchange: 'anypay.events',
+
+    routingkey: 'models.Invoice.afterUpdate',
+
+    queue: 'republish_invoice_updated_as_account_id'
+
+  })
+  .start(async (channel, msg, json) => {
+
+    let routingKey = `accounts.${json.account_id}.invoiceupdated`;
 
     await channel.publish('anypay.events', routingKey, msg.content);
 
