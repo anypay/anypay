4b40a807923d9416f5dc34c0e7c92a41019783b8
Steven Zeiler <steven@anypayinc.com>
2019-10-16 11:03:09 -0400

    plugins transform address work (#483)
    for several coins. good stuff

---
+++ bin/polynym.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env ts-node
+
+import * as polynym from 'polynym';
+
+  const address = '$zyler';
+
+import * as program from 'commander';
+
+program
+  .command('resolve <address>')
+  .action(async (address) => {
+
+    polynym.resolveAddress(address).then(x => {
+        console.log(x);
+    }).catch(e=>{
+        console.error(e);
+    });
+  
+  });
+  
+program
+  .parse(process.argv);
+
