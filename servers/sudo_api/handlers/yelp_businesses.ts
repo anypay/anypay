268e0fa3652cf44d9356ac69269497df148c24ac
Steven Zeiler <steven@anypayinc.com>
2019-10-31 03:07:45 -0400

    sudo endpoint to return all 5000+ yelp businesses (#502)

--- servers/sudo_api/handlers/yelp_businesses.ts
+++ servers/sudo_api/handlers/yelp_businesses.ts
@@ -1,13 +1,19 @@
 
 import { models } from '../../../lib';
+import { getMongodb } from '../../../lib/mongodb';
+import { Readable } from 'stream';
 
 import { badRequest } from 'boom';
 
 export async function index(req, h) {
 
+  let mongodb = await getMongodb();
+
   try {
 
-    return models.YelpBusiness.findAll();
+    //return models.YelpBusiness.findAll();
+
+    return  mongodb.collection('yelp_businesses').find().toArray();
 
   } catch(error) {
 
@@ -16,3 +22,4 @@ export async function index(req, h) {
   }
 
 }
+
