ab8169debdac769ce7dc565f2e335d5b9dd67904
Steven Zeiler <me@stevezeiler.com>
2019-11-13 06:42:21 -0500

    payroll batch modelling

--- migrations/20191113103100-create-payroll-account.js
+++ migrations/20191113103100-create-payroll-account.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: (queryInterface, Sequelize) => {
     /*
     return queryInterface.createTable('PayrollAccounts', {
       id: {
