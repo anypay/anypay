71cd69d361d90dc2cf58e19d86f67b55cc10706b
brandonbryant12 <brandonbryant002@gmail.com>
2019-11-27 14:22:17 -0500

    set direct price for DASH/VEF

--- test/seed_prices.ts
+++ test/seed_prices.ts
@@ -7,7 +7,7 @@ import { prices } from '../lib';
   await prices.setPrice('BTC', 1000, 'test', 'USD');
   await prices.setPrice('BCH', 10,'test', 'USD');
   await prices.setPrice('DASH', 20,'test', 'USD');
-  await prices.setPrice('VEF', 20,'test', 'DASH');
+  await prices.setPrice('DASH', 20,'test', 'VEF');
   await prices.setPrice('VEF', 0.0000005,'test', 'USD');
 
   process.exit(0);
