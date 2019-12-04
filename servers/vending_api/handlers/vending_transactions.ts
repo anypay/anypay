38af64027136b6bebc24903d26251c4a0950c4d7
brandonbryant12 <brandonbryant002@gmail.com>
2019-12-04 15:18:01 -0500

    vending api service

---
+++ servers/vending_api/handlers/vending_transactions.ts
@@ -0,0 +1,47 @@
+import {models} from '../../../lib';
+console.log(models)
+export async function getAccountTransactions(request, h) {
+
+  let accountId = request.auth.credentials.accessToken.account_id;
+
+  console.log(accountId)
+
+  let machine = await models.VendingMachine.findOne({where: { account_id: accountId }})
+  console.log(machine.toJSON())
+
+  if( machine ){
+
+    let records = await models.VendingTransaction.findAll({
+      where : { 'terminal_id' : machine.serial_number},
+      order: [ [ 'terminal_time', 'DESC' ]]      
+    });
+
+    return records
+
+  }
+
+  return 'No Vending Machine Associated With Account';
+
+}
+
+export async function getLatestTransactions(request, h) {
+
+  let records = await models.VendingTransaction.findAll({
+    limit: 100,
+    order: [ [ 'terminal_time', 'DESC' ]]       
+  });
+
+  return records
+
+}
+
+export async function getMachineTransactions(request, h) {
+
+  let records = await models.VendingTransaction.findAll({
+    where : { 'terminal_id' : request.params.serial_number},
+    order: [ [ 'terminal_time', 'DESC' ]]       
+  });
+
+  return records
+
+}
---
+++ servers/vending_api/server.ts
@@ -0,0 +1,116 @@
+require('dotenv').config();
+import * as Hapi from "hapi";
+const Joi = require('joi');
+import { log } from '../../lib';
+import { models } from '../../lib'
+const VendingMachineTransactions = require("./handlers/vending_transactions");
+
+import * as auth from './auth';
+
+
+
+const kBadRequestSchema = Joi.object({
+  statusCode: Joi.number().integer().required(),
+  error: Joi.string().required(),
+  message: Joi.string().required()
+}).label('BoomError')
+
+function responsesWithSuccess({ model }) {
+  return {
+    'hapi-swagger': {
+      responses: {
+        200: {
+          description: 'Success',
+          schema: model
+        },
+        400: {
+          description: 'Bad Request',
+          schema: kBadRequestSchema,
+        },
+        401: {
+          description: 'Unauthorized',
+          schema: kBadRequestSchema,
+        },
+      },
+    },
+  }
+}
+
+async function Server() {
+
+  var server = new Hapi.Server({
+    host: process.env.HOST || "localhost",
+    port: process.env.PORT || 8200,
+    routes: {
+      cors: true,
+      validate: {
+        options: {
+          stripUnknown: true
+        }
+      }
+    }
+  });
+
+  await server.register(require('hapi-auth-basic'));
+  server.auth.strategy("token", "basic", { validate: auth.validateToken });
+  server.auth.strategy("sudopassword", "basic", { validate: auth.validateSudoPassword});
+
+  server.route({
+
+    method: 'GET',
+
+    path: '/api/vending/transactions/{serial_number}',
+
+    config: { auth : "sudopassword" },
+
+    handler: VendingMachineTransactions.getMachineTransactions
+
+  })
+
+  server.route({
+
+    method: 'GET',
+
+    path: '/api/vending/transactions',
+
+    config: { auth : "sudopassword" },
+
+    handler:  VendingMachineTransactions.getLatestTransactions
+
+  })
+
+  server.route({
+
+    method: 'GET',
+
+    path: '/api/vending/account/transactions',
+
+    config: { auth : "token" },
+
+    handler: VendingMachineTransactions.getAccountTransactions
+
+  })
+
+  return server;
+
+}
+
+
+if (require.main === module) {
+
+  start();
+}
+
+async function start () {
+
+  var server = await Server();
+
+  // Start the server
+  await server.start();
+
+  log.info("Server running at:", server.info.uri);
+
+}
+
+export { Server, start }
+
