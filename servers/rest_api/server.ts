2a94be507f4a10f9847aa0a4254a918e0309e4d3
Steven Zeiler <me@stevezeiler.com>
2020-01-04 16:02:57 -0500

    api endpoint to list kiosk transactions

--- servers/rest_api/server.ts
+++ servers/rest_api/server.ts
@@ -1428,6 +1428,16 @@ async function Server() {
 
   server.route({
     method: "GET",
+    path: "/kiosks/{kiosk_id}/transactions",
+    options: {
+      auth: "token",
+      tags: ['api'],
+      handler: handlers.KioskTransactions.index
+    }
+  });
+
+  server.route({
+    method: "GET",
     path: "/square/locations",
     options: {
       auth: "token",
