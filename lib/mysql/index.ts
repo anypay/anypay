643372b836f44d591670d13c0d1b77d173ccf1a5
brandonbryant12 <brandonbryant002@gmail.com>
2020-01-09 13:43:46 -0500

    throw error when no vending machine found

--- lib/mysql/index.ts
+++ lib/mysql/index.ts
@@ -108,51 +108,54 @@ export async function writeTransactionRecord(event){
   let accountId, vendingMachineId; 
 
   if( vendingMachine){
+
     accountId = vendingMachine.account_id;
     vendingMachineId = vendingMachine.id;
+
+    if( newRow.type === 0){
+      newRow.type = 'BUY';
+    }
+    if( newRow.type === 1){
+      newRow.type = 'SELL';
+    }
+
+ 
+    let record = {
+      account_id: accountId,
+      vending_machine_id: vendingMachineId,
+      terminal_id: newRow.terminal_id,
+      server_time: new Date(event.timestamp),
+      terminal_time: new Date(event.timestamp),
+      localtid: newRow.localtid,
+      remotetid: newRow.remotetid,
+      type: newRow.type,
+      cash_amount: newRow.cashamount,
+      cash_currency: newRow.cashcurrency,
+      crypto_currency: newRow.cryptocurrency,
+      crypto_amount: newRow.cryptoamount,
+      crypto_address: newRow.cryptoaddress,
+      status: newRow.status,
+      hash: newRow.detail,
+      exchange_price: (newRow.ratesourceprice * (1+ newRow.expectedprofitsetting/100)).toFixed(2),
+      spot_price: (newRow.ratesourceprice).toFixed(2),
+      fixed_transaction_fee: (newRow.fixedtransactionfee).toFixed(2),
+      expected_profit_setting: (newRow.expectedprofitsetting).toFixed(2),
+      expected_profit_value: (newRow.expectedprofitvalue).toFixed(2),
+      name_of_crypto_setting_used: newRow.nameofcryptosettingused,
+      additional_output_strategy: vendingMachine.additional_output_strategy_id
+    }
+    let tx = await models.VendingTransaction.findOrCreate({
+      where:{
+        terminal_id: record.terminal_id.toString(),
+        hash : record.hash,
+        crypto_address: record.crypto_address
+      },
+      defaults: record
+    })
+
+    return tx
+
   }
 
-  if( newRow.type === 0){
-    newRow.type = 'BUY';
-  }
-  if( newRow.type === 1){
-    newRow.type = 'SELL';
-  }
-
-
-  let record = {
-    account_id: accountId,
-    vending_machine_id: vendingMachineId,
-    terminal_id: newRow.terminal_id,
-    server_time: new Date(event.timestamp),
-    terminal_time: new Date(event.timestamp),
-    localtid: newRow.localtid,
-    remotetid: newRow.remotetid,
-    type: newRow.type,
-    cash_amount: newRow.cashamount,
-    cash_currency: newRow.cashcurrency,
-    crypto_currency: newRow.cryptocurrency,
-    crypto_amount: newRow.cryptoamount,
-    crypto_address: newRow.cryptoaddress,
-    status: newRow.status,
-    hash: newRow.detail,
-    exchange_price: (newRow.ratesourceprice * (1+ newRow.expectedprofitsetting/100)).toFixed(2),
-    spot_price: (newRow.ratesourceprice).toFixed(2),
-    fixed_transaction_fee: (newRow.fixedtransactionfee).toFixed(2),
-    expected_profit_setting: (newRow.expectedprofitsetting).toFixed(2),
-    expected_profit_value: (newRow.expectedprofitvalue).toFixed(2),
-    name_of_crypto_setting_used: newRow.nameofcryptosettingused,
-    additional_output_strategy: vendingMachine.additional_output_strategy_id
-  }
-  let tx = await models.VendingTransaction.findOrCreate({
-    where:{
-      terminal_id: record.terminal_id.toString(),
-      hash : record.hash,
-      crypto_address: record.crypto_address
-    },
-    defaults: record
-  })
-
-  return tx
-
+  throw new Error(`No vending machine record associated with terminal id: ${newRow.id}`)
 }
