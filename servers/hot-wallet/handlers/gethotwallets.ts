ba104a99714d67ba2e6e1971c92e8c1849f6324a
brandonbryant12 <brandon.bryant002@gmail.com>
2020-01-24 11:37:41 -0500

    Dash wallet (#573)
    * install knex and import models from lib
    
    * dash hot wallet plugin and add it to the hot wallet server

--- servers/hot-wallet/handlers/gethotwallets.ts
+++ servers/hot-wallet/handlers/gethotwallets.ts
@@ -1,15 +1,27 @@
 import * as bchWallet from '../../../plugins/bch/wallet';
-import * as btcWallet from '../../../plugins/btc/wallet';
+import * as btcWallet from '../../../plugins/btc/wallet';	
+import * as dashWallet from '../../../plugins/dash/wallet';
+import * as Boom from 'boom';
+
 
 export async function getHotWallets(){
 
-  let btc = await btcWallet.getHotWallet();
+  try{
 
-  let bch = await bchWallet.getHotWallet();
+    let btc = await btcWallet.getHotWallet()
 
-  let wallets = [btc, bch]
+    let bch = await bchWallet.getHotWallet()
 
-  return {wallets}
+    let dash = await dashWallet.getHotWallet();
+
+    let wallets = [btc, bch, dash]	   
+
+    return {wallets}    
+
+  }catch(error){
+
+    return Boom.badRequest(error)
+
+  } 
 
 }
-
