f2b57555a055769c74837133fee0bb35ebbc0615
Steven Zeiler <steven@anypayinc.com>
2020-02-04 22:30:41 -0500

    Sudo API for recording, viewing cash deposits (#593)
    * sudo api for cash deposits
    
    * Joi.number not Joi.integer

---
+++ migrations/20200205031957-create-cash-deposit.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('cash_deposits', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('cash_deposits');
+  }
+};
