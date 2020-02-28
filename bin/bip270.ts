8303437ef2dde84693be3d81370cf5300a8a76ac
Steven Zeiler <steven@anypayinc.com>
2020-02-28 00:47:09 -0500

    Bip270 (#605)
    * bip270 payment requests for Bitcoin SV
    
    * command line tool to auto-pay an invoice using
    the bip270 payment protocol
    
    * update bip270 docs with recommendations from Brandon
    and the protocol sequence image from money button
    
    * log full payment protocol request to inspect headers
    
    * handle both dash and bsv payment requests at the
    same endpoint using http headers. implement bip270
    and bip271 for both.

---
+++ bin/bip270.ts
@@ -0,0 +1,77 @@
+#!/usr/bin/env ts-node
+require('dotenv').config()
+
+import * as program from 'commander';
+import * as http from 'superagent';
+import * as bitcoin from 'bsv';
+import * as datapay from 'datapay';
+
+program
+  .command('payinvoice <invoice_uid>')
+  .action(async (invoiceUID) => {
+
+
+    let response = await http
+    //.get(`https://api.anypayinc.com/r/${invoiceUID}`)
+      .get(`http://localhost:8000/r/${invoiceUID}`)
+      .set({
+        'accept': 'application/bitcoinsv-paymentrequest'
+      })
+
+    console.log(response.body);
+
+    let outputs = response.body.outputs.map(output => {
+
+      console.log('output', output);
+
+      let script = new bitcoin.Script(output.script);
+
+      let address = new bitcoin.Address(script.getPublicKeyHash()).toString();
+
+      let value = output.amount;
+
+      return {
+        address,
+        value
+      }
+
+    });
+
+    var config = {
+      pay: {
+        key: process.env.BIP_270_EXTRA_OUTPUT_WIF,
+        to: outputs
+      }
+    }
+    datapay.send(config, console.log);
+
+  });
+
+program
+  .command('getpaymentrequest <invoice_uid> <currency>')
+  .action(async (invoiceUID, currency) => {
+
+    var accept;
+
+    switch(currency) {
+    case 'BSV':
+      accept = 'application/bitcoinsv-paymentrequest'
+      break;
+    case 'DASH':
+      accept = 'application/dash-paymentrequest'
+      break;
+    }
+
+    let response = await http
+      .get(`http://127.0.0.1:8000/r/${invoiceUID}`)
+      .set({
+        'accept': accept
+      });
+
+    console.log(response);
+
+  });
+
+
+program.parse(process.argv);
+
