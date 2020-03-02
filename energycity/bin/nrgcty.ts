cb3baa0bf877c156b723a1b50f0cc90064a88289
Steven Zeiler <steven@anypayinc.com>
2020-03-02 06:58:27 -0500

    make cities in energy city dynamic from database (#608)
    update cities through sudo api

--- energycity/bin/nrgcty.ts
+++ energycity/bin/nrgcty.ts
@@ -3,6 +3,28 @@
 import * as program from 'commander';
 
 import { listCities } from '../lib';
+import { models } from '../../lib';
+
+program
+  .command('addcity <name> <stub>')
+  .action(async (name, stub) => {
+
+    try {
+
+      let city = await models.City.create({
+        name,
+        stub,
+        tag: `city:${stub}`
+      });
+
+      console.log(city.toJSON());
+
+      process.exit(0);
+    } catch(error) {
+      console.log(error);
+    }
+
+  });
 
 program
   .command('listcities')
