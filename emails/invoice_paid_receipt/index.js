db6d833e5ab10d03a1289c2eb1d75ddb945ed73d
Steven Zeiler <me@stevezeiler.com>
2020-03-10 15:01:44 -0400

    actor & library to send beautify invoice receipt email

--- emails/invoice_paid_receipt/index.js
+++ emails/invoice_paid_receipt/index.js
@@ -1,18 +1,13 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import * as Handlebars from 'handlebars';
-
-let body = readFileSync(join(__dirname, 'email.html'));
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const path_1 = require("path");
+const Handlebars = require("handlebars");
+let body = fs_1.readFileSync(path_1.join(__dirname, 'email.html'));
 let template = Handlebars.compile(body.toString());
-
-export default {
-
-  title: "Anypay Payment Received - Invoice Receipt",
-
-  template,
-
-  body: template({})
-
-}
-
+exports.default = {
+    title: "Payment Received with Anypay",
+    template,
+    body: template({})
+};
+//# sourceMappingURL=index.js.map
