898466507958487ed5f7bbcc18998cfa556e4d24
Steven Zeiler <steven@anypayinc.com>
2020-03-31 14:46:15 -0400

    Bsv allow paymentack (#646)
    * library to abstract parsing sequelize options
    from a hapi request query object
    
    * add command to reroute all payments for currency
    
    * allow headers for bitcoinsv-paymentack
    also return error with 400 status code
    
    Co-authored-by: Steven Zeiler <me@stevezeiler.com>

--- bin/rectify.ts
+++ bin/rectify.ts
@@ -3,8 +3,9 @@
 import * as program from 'commander';
 
 import { rpc } from '../plugins/dash/lib/jsonrpc';
-import { channel, awaitChannel } from '../lib/amqp';
+import { channel, awaitChannel, wait } from '../lib/amqp';
 import { rawTxToPayment } from '../plugins/dash/lib';
+import { database } from '../lib';
 
 program
   .command('rectifydash <hash>')
@@ -39,6 +40,29 @@ program
       console.error(error);
 
     }
+
+  });
+
+program
+  .command('reroutefailed <currency>')
+  .action(async (currency) => {
+
+    let result = await database.query(`select * from invoices where status != 'unpaid'
+    and output_hash is null and currency = '${currency}' order by "createdAt" desc limit 100;`);
+
+    let exchange = `${currency.toLowerCase()}.anypay.global`;
+
+    let channel = await awaitChannel();
+
+    for (let i =0; i < result[0].length; i++) {
+      let invoice = result[0][i];
+      await wait(100);
+
+
+      await channel.publish(exchange, 'walletnotify', Buffer.from(invoice.hash));
+    }
+
+    process.exit(0);
 
   });
 
