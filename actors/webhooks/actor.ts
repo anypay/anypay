d40bc645d4150ffc37a3e569af884dab25c06c1e
Steven Zeiler <steven@anypayinc.com>
2020-05-29 12:34:50 -0400

    Fix webhook handler (#695)
    * move webhooks route to handler
    return model record to webhook caller
    
    * in webhooks actor use same sendwebhook function as
    the sudo api manual resubmission handler
    
    Co-authored-by: Steven Zeiler <me@stevezeiler.com>

--- actors/webhooks/actor.ts
+++ actors/webhooks/actor.ts
@@ -1,85 +1,42 @@
 require('dotenv').config();
 
-import { log, models } from '../../lib';
+import { log } from '../../lib';
 
 import { Actor } from 'rabbi'
 
-import * as http from 'superagent';
+import { sendWebhookForInvoice } from '../../lib/webhooks';
 
-import * as validate from 'validator';
-
-async function start() {
+export async function start() {
 
   Actor.create({
+
     exchange: 'anypay:invoices',
+
     bindingkey: 'invoice:paid',
+
     queue: 'webhooks.invoice.tosend'
+
   })
   .start(async (channel, msg) => {
+
     let uid = msg.content.toString();
 
-    let invoice = await models.Invoice.findOne({ where: { uid }});
-
-    if (!invoice) {
-
-      log.error(`invoice ${uid} not found`);
-
-      channel.ack(msg);
-
-      return;
-
-    }
-
-    var valid;
-
     try {
-      console.log(invoice.webhook_url);
 
-      valid = validate.isURL(invoice.webhook_url)
+      let webhook = await sendWebhookForInvoice(uid);
+
+      console.log('webhook.sent', webhook.toJSON());
 
     } catch(error) {
 
-      valid = false;
+      log.error('webhook.failed', error.message);
 
     }
 
-    if (valid) {
-
-      try {
-
-        log.info(`sending webhook for ${uid}`);
-
-        let resp = await http.post(invoice.webhook_url).send(invoice.toJSON()); 
-
-        log.info('webhook.sent', invoice.toJSON());
-
-        channel.ack(msg);
-
-      } catch(error) {
-
-        log.error('webhook.failed', error.message);
-
-        channel.ack(msg);
-
-      }
-
-
-    } else {
-
-      log.error('invalid webhook url', invoice.webhook_url);
-
-      channel.ack(msg);
-
-    }
+    await channel.ack(msg);
 
   });
 
-
-}
-
-export {
-
-  start
 
 }
 
