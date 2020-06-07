74e02f18115746301331d11a4cff21627f1e4321
Steven Zeiler <me@stevezeiler.com>
2020-06-07 18:19:18 -0400

    Handle Edge payment requests (BitPay JSON V2) For DASH,BCH,BSV,BTC

--- servers/grab-and-go/handlers/payment_requests.ts
+++ servers/grab-and-go/handlers/payment_requests.ts
@@ -8,6 +8,10 @@ import {generatePaymentRequest as createDASHRequest} from '../../../plugins/dash
 
 import { models, invoices } from '../../../lib';
 
+import { BigNumber } from 'bignumber.js';
+
+import * as moment from 'moment';
+
 export async function create(req: Hapi.Request, h) {
 
   console.log('params', req.params);
@@ -27,121 +31,136 @@ export async function create(req: Hapi.Request, h) {
     currency = 'BSV';
   } 
 
-  try {
+  let account = await models.Account.findOne({
+    where: {
+      stub: req.params.account_stub
+    }
+  });
 
-    let account = await models.Account.findOne({
-      where: {
-        stub: req.params.account_stub
-      }
-    });
+  console.log('account', account.toJSON());
 
-    console.log('account', account.toJSON());
+  // look up the item from the url parameters
+  var item = await models.GrabAndGoItem.findOne({
 
-    // look up the item from the url parameters
-    var item = await models.GrabAndGoItem.findOne({
-
-      where: {
-        stub: req.params.item_stub,
-        account_id: account.id 
-      }
-
-    });
-
-    console.log('ITEM', item.toJSON());
-
-    if (!item) {
-      throw new Error(`item ${req.params.item_stub} for account not found`);
+    where: {
+      stub: req.params.item_stub,
+      account_id: account.id 
     }
 
-    let invoice = await invoices.generateInvoice(account.id, item.price, currency);
+  });
 
-    console.log('INVOICE CREATED', invoice.toJSON());
+  console.log('ITEM', item.toJSON());
 
-    if (item.square_catalog_object_id) {
-
-      console.log('SQUARE OBJECT ID', item.square_catalog_object_id);
-
-      invoice.external_id = item.square_catalog_object_id;
-
-      await invoice.save();
-
-    } else {
-
-      console.log('item no object id', item.toJSON());
-    
-    }
-
-    await models.GrabAndGoInvoice.create({
-
-      invoice_uid: invoice.uid,
-
-      item_id: item.id
-
-    });
-
-    let paymentRequest = await generatePaymentRequest(invoice, account);
-
-    const response = h.response(paymentRequest.serialize());
-
-    response.type('application/bitcoincash-paymentrequest');
-    response.header('Content-Type', 'application/bitcoincash-paymentrequest');
-    response.header('Accept', 'application/bitcoincash-payment');
-
-    return response;
-
-  } catch(error) {
-
-    console.log(error);
-    console.log(error.message);
-
-    return Boom.badRequest(error.message);
-
+  if (!item) {
+    throw new Error(`item ${req.params.item_stub} for account not found`);
   }
+
+  let invoice = await invoices.generateInvoice(account.id, item.price, currency);
+
+  console.log('INVOICE CREATED', invoice.toJSON());
+
+  if (item.square_catalog_object_id) {
+
+    console.log('SQUARE OBJECT ID', item.square_catalog_object_id);
+
+    invoice.external_id = item.square_catalog_object_id;
+
+    await invoice.save();
+
+  } else {
+
+    console.log('item no object id', item.toJSON());
+  
+  }
+
+  await models.GrabAndGoInvoice.create({
+
+    invoice_uid: invoice.uid,
+
+    item_id: item.id
+
+  });
+
+  let paymentRequest = await generatePaymentRequest(invoice, account);
+
+  const response = h.response(paymentRequest.serialize());
+
+  response.type('application/bitcoincash-paymentrequest');
+  response.header('Content-Type', 'application/bitcoincash-paymentrequest');
+  response.header('Accept', 'application/bitcoincash-payment');
+
+  return response;
 
 }
 
-export async function createByItemUid(req: Hapi.Request, h) {
+async function handleEdge(req: Hapi.Request, h: Hapi.ResponseToolkit) {
 
-  console.log('params', req.params);
-  console.log('payment protcol full request:');
-  console.log(req);
+  console.log('edgewallet.detected'); 
 
-  var currency;
+  let currency = req.headers['x-currency'];
 
-  if (req.headers.accept === 'application/dash-paymentrequest') {
-    currency = 'DASH';
-  } else if (req.headers.accept === 'application/bitcoincash-paymentrequest') {
-    currency = 'BCH';
-  } else {
-    currency = 'BSV';
-  } 
+  if (!currency) {
+    //throw new Error('x-currency header must be provided with value such as BCH,DASH,BSV')
+    currency = 'DASH'
+  }
 
-  // https://anypayinc.com/grab-and-go/freshpress-portsmouth/green-on-fleet/purchase
-  // /grab-and-go/:account_stub/:item_stub/purchase
+  let { invoice, item } = await createGrabAndGoInvoice(req.params.item_uid, currency);
 
-  try {
+  let amount = new BigNumber(invoice.amount);
 
+  if (invoice.address.match(/\:/)) {
+    invoice.address = invoice.address.split(':')[1];
+  }
+
+  const paymentRequest = {
+    "network": "main",
+    "currency": invoice.currency,
+    "requiredFeeRate": 1,
+    "outputs": [
+        {
+            "amount": amount.times(100000000).toNumber(),
+            "address": invoice.address
+        }
+    ],
+    "time": moment().toDate(),
+    "expires": moment().add(15, 'minutes').toDate(),
+    "memo": `Payment request for Anypay invoice ${invoice.uid}`,
+    "paymentUrl": `https://anypayinc.com/payments/edge/${invoice.currency}/${invoice.uid}`,
+    "paymentId": invoice.uid
+  }
+
+  console.log(paymentRequest);
+
+  let response = h.response(paymentRequest);
+
+  response.type('application/payment-request');
+
+  response.header('Content-Type', 'application/payment-request');
+
+  response.header('Accept', 'application/payment');
+
+  return response;
+
+}
+
+async function createGrabAndGoInvoice(item_uid: string, currency: string) {
     // look up the item from the url parameters
     let item = await models.GrabAndGoItem.findOne({
 
       where: {
-        uid: req.params.item_uid
+        uid: item_uid
       }
 
     });
 
-    console.log('item', item.toJSON());
-
     if (!item) {
-      throw new Error(`item ${req.params.item_uid} for account not found`);
+      throw new Error(`item ${item_uid} for account not found`);
     }
 
     let invoice = await invoices.generateInvoice(item.account_id, item.price, currency);
 
     if (item.square_catalog_object_id) {
 
-      console.log('SQUARE OBJECT ID', item.square_catalog_object_id);
-
       invoice.external_id = item.square_catalog_object_id;
 
       await invoice.save();
@@ -159,6 +178,42 @@ export async function createByItemUid(req: Hapi.Request, h) {
       item_id: item.id
 
     });
+
+    return { invoice, item };
+
+
+}
+
+export async function createByItemUid(req: Hapi.Request, h: Hapi.ResponseToolkit) {
+  var isEdge = false;
+
+  /* Detect EDGE Wallet for Bitpay's JSON Protocol Support */
+
+
+  if (req.headers['x-requested-with'] === 'co.edgesecure.app') {
+
+    return handleEdge(req, h);
+
+  }
+
+  console.log("HEADERS", req.headers);
+
+  var currency;
+  if (req.headers.accept === 'application/bitcoin-paymentrequest') {
+    currency = 'BTC';
+  } else if (req.headers.accept === 'application/dash-paymentrequest') {
+    currency = 'DASH';
+  } else if (req.headers.accept === 'application/bitcoincash-paymentrequest') {
+    currency = 'BCH';
+  } else if (req.headers.accept === 'application/simpleledger-paymentrequest') {
+    currency = 'BCH';
+  } else {
+    currency = 'BSV';
+  } 
+
+  try {
+
+    let { invoice, item } = await createGrabAndGoInvoice(req.params.item_uid, currency);
 
     let account = await models.Account.findOne({ where: { id: item.account_id }});
 
@@ -190,6 +245,20 @@ export async function createByItemUid(req: Hapi.Request, h) {
       response.header('Content-Type', 'application/dash-paymentrequest');
 
       response.header('Accept', 'application/dash-payment');
+
+      return response;
+
+    case 'BTC':
+
+      paymentRequest = await generatePaymentRequest(invoice, account);
+
+      response = h.response(paymentRequest.serialize());
+
+      response.type('application/bitcoin-paymentrequest');
+
+      response.header('Content-Type', 'application/bitcoin-paymentrequest');
+
+      response.header('Accept', 'application/bitcoin-payment');
 
       return response;
 
