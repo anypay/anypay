d8a0772b87aeda8b5773ad63b30bc11e087b591e
Steven Zeiler <me@stevezeiler.com>
2020-07-03 14:12:56 -0400

    log BIP70 Payment message request for DASH

--- servers/rest_api/handlers/dash_payment_request_protobuf.ts
+++ servers/rest_api/handlers/dash_payment_request_protobuf.ts
@@ -1,11 +1,23 @@
 import {generatePaymentRequest} from '../../../plugins/dash/lib/paymentRequest';
+import * as Hapi from 'hapi';
 
 import { rpc } from '../../../plugins/dash/lib/jsonrpc'
 
 const bitcoin = require('bsv'); 
 const Message = require('bsv/message'); 
 
+function isCorrectContentType(req: Hapi.Request) {
+  return req.headers['x-content-type'] === 'application/dash-payment'
+}
+
+function isCorrectAccept(req: Hapi.Request) {
+  return req.headers['x-accept'] === 'application/dash-paymentack'
+}
+
 export async function create(req, h) {
+
+  console.log('REQUEST');
+  console.log(req);
 
   console.log('HEADERS');
   console.log(req.headers);
@@ -13,47 +25,13 @@ export async function create(req, h) {
   console.log('PAYLOAD');
   console.log(req.payload);
 
-  if (req.headers.accept === 'application/verify-payment') {
+  if (isCorrectContentType(req) || isCorrectAccept(req)) {
+
+    console.log('content-type or accept is correct for posting DASH BIP70 Payments');
 
     return {
 
-      payment: req.payload,
-
-      memo: "This looks good for now, we will see what the miners say."
-
-    }
-
-  }
-
-  if (req.headers['x-content-type'] === 'application/payment') {
-
-    req.payload.transactions.forEach(async (hex) => {
-
-      console.log("BROADCAST", hex);
-
-      try {
-
-        let resp = await rpc.call('sendrawtransaction', [hex]);
-
-        console.log('resp', resp);
-
-      } catch(error) {
-
-        console.log('could not broadcast transaction', hex);
-
-      }
-
-    });
-
-    return {
-
-      payment: {
-
-        transactions: req.payload.transactions 
-
-      },
-
-      memo: "Transaction received by Anypay. Invoice will be marked as paid if the transaction is confirmed."
+      success: true
 
     }
 
