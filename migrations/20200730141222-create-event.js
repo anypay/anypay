86ceae4eb9b3c0b3067ac253949b9e8a58b7fd52
Steven Zeiler <steven@anypayinc.com>
2020-07-30 13:31:47 -0400

    log to database, slack when someone logs in (#726)
    Co-authored-by: Steven Zeiler <me@stevezeiler.com>

---
+++ migrations/20200730141222-create-event.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      account_id: {
+        type: Sequelize.INTEGER
+      },
+      event: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      payload: {
+        type: Sequelize.JSON,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('events');
+  }
+};
