32a6ab6a711e8bbc32e9f567b5ec70c1c9cb7556
Steven Zeiler <me@stevezeiler.com>
2020-08-18 21:33:48 -0400

    Allow Account To Remove Ambassador Return Correct Ambassador Data To Admin App

--- lib/accounts/index.ts
+++ lib/accounts/index.ts
@@ -33,6 +33,8 @@ export async function updateAccount(account, payload) {
 
   let updateAttrs: any = Object.assign(payload, {});
 
+  log.info('account.update', updateAttrs)
+
   if (updateAttrs.physical_address) {
 
     try {
@@ -75,6 +77,7 @@ export async function updateAccount(account, payload) {
     })
 
     if (ambassador) {
+      log.info('ambassador.found', { ambassador })
 
       updateAttrs['ambassador_id'] = ambassador.id;
 
@@ -91,6 +94,11 @@ export async function updateAccount(account, payload) {
   }
 
   delete updateAttrs['ambassador_email'];
+
+  if (updateAttrs.remove_ambassador) {
+    updateAttrs['ambassador_id'] = null
+    delete updateAttrs['remove_ambassador']
+  }
 
   await models.Account.update(updateAttrs, {
 
