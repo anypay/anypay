3b78c337700235afb67e06bbca8c0afabe708563
Steven Zeiler <me@stevezeiler.com>
2020-09-18 11:58:45 -0400

    API to get Stats on BATM Sales

--- servers/sudo_api/server.ts
+++ servers/sudo_api/server.ts
@@ -364,6 +364,38 @@ async function Server() {
 
     method: "GET",
 
+    path: "/api/batm_sales",
+
+    config: {
+
+      auth: "sudopassword",
+
+      handler: handlers.BatmSales.index
+
+    }
+
+  });
+
+  server.route({
+
+    method: "GET",
+
+    path: "/api/batm_sales/stats",
+
+    config: {
+
+      auth: "sudopassword",
+
+      handler: handlers.BatmSales.stats
+
+    }
+
+  });
+
+  server.route({
+
+    method: "GET",
+
     path: "/api/vending_machines/{id}",
 
     config: {
