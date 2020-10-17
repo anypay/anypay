636639dab28175d90e81f5990317dc26f97807f2
Steven Zeiler <steven@anypayinc.com>
2020-10-17 19:38:05 -0400

    Receive and Record Clover Webhooks (#787)
    Co-authored-by: Steven Zeiler <me@stevezeiler.com>

---
+++ migrations/20201017202123-create-clover-webhook.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('CloverWebhooks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      payload: {
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('CloverWebhooks');
+  }
+};
