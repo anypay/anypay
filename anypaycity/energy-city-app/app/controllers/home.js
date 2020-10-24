64f6d57ba2dfce8616cba2e130fd92f6e9c8b28d
Steven Zeiler <steven@anypayinc.com>
2020-10-24 16:48:40 -0400

    Anypaycity current invoice (#790)
    * API To Display Current Invoice (Unpaid Less Than 1 Minute)
    
    * Get Nearby Locations Or Search By City
    
    * Automatically Use Location If Available
    
    Co-authored-by: Steven Zeiler <me@stevezeiler.com>

---
+++ anypaycity/energy-city-app/app/controllers/home.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  actions: {
+
+    async findNearby() {
+      console.log("find nearby")
+
+      let permission = await window.navigator.permissions.query({name:'geolocation'})
+
+      if (permission.state === 'granted') {
+
+      } else {
+
+        //alert(permission.state)
+      }
+
+      if (permission.state === 'granted') {
+
+      } else {
+
+      }
+
+      $('#loader-wrapper').show()
+      window.navigator.geolocation.getCurrentPosition((position) => {
+
+        $('#loader-wrapper').hide()
+        console.log('geolocation.currentposition', position)
+
+        this.transitionToRoute('map', position.coords.latitude, position.coords.longitude)
+      
+      }, (error) => {
+        $('#loader-wrapper').hide()
+        console.log('geolocation.error', error)
+
+        this.transitionToRoute('search-city')
+
+      
+      }, {
+        enableHighAccuracy: false 
+      });
+    }
+  }
+});
+
+function revokePermission() {
+  return window.navigator.permissions.revoke({name:'geolocation'})
+}
---
+++ anypaycity/energy-city-app/app/routes/home.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  async setupController() {
+
+    let permission = await window.navigator.permissions.query({name:'geolocation'})
+
+    if (permission.state === 'granted') {
+
+      $('#loader-wrapper').show()
+      window.navigator.geolocation.getCurrentPosition((position) => {
+
+        $('#loader-wrapper').hide()
+        console.log('geolocation.currentposition', position)
+
+        this.transitionTo('map', position.coords.latitude, position.coords.longitude)
+      
+      }, (error) => {
+        $('#loader-wrapper').hide()
+        console.log('geolocation.error', error)
+
+        this.transitionToRoute('search-city')
+
+      
+      }, {
+        enableHighAccuracy: false 
+      });
+
+
+    }
+
+  }
+});
+
+
