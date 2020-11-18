8d98452d0880be3d9292bda50a104b37a4dfd7c2
Steven Zeiler <me@stevezeiler.com>
2020-11-18 13:19:33 -0500

    Set Prices In Invoice Test Before Running

--- test/lib/invoice_test.ts
+++ test/lib/invoice_test.ts
@@ -5,6 +5,9 @@ import { replaceInvoice } from '../../lib/invoice';
 import { settleInvoice } from '../../lib/invoice';
 import { registerAccount, setAddressScalar } from '../../lib/accounts';
 import { setAddress, setDenomination } from '../../lib/core';
+
+import { updateCryptoUSDPrices } from '../../lib/prices/crypto';
+
 import * as assert from 'assert';
 
 import * as Chance from 'chance';
@@ -13,6 +16,12 @@ import * as moment from 'moment';
 const chance = new Chance();
 
 describe("Creating Invoices", () => {
+
+  before(async () => {
+    
+    await updateCryptoUSDPrices()
+
+  })
 
   it("#generateInvoice should create a new DASH invoice", async () => {
 
