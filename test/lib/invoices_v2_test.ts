1e179653f3d088dbd5a39124735c8949581da38b
Steven Zeiler <me@stevezeiler.com>
2020-11-24 18:57:00 -0500

    Use assert.strictEqual rather than ===

--- test/lib/invoices_v2_test.ts
+++ test/lib/invoices_v2_test.ts
@@ -33,7 +33,7 @@ describe("Invoices V2", () => {
       assert.strictEqual(invoice.currency, 'USD')
       assert.strictEqual(invoice.amount, 10)
 
-      assert(invoice.payment_options.length === 0)
+      assert.strictEqual(invoice.payment_options.length, 0)
       assert(invoice.uid)
       assert(invoice.uri)
       assert(invoice.web_url)
@@ -49,7 +49,7 @@ describe("Invoices V2", () => {
         amount: 10
       })
 
-      assert(invoice.payment_options.length === 1)
+      assert.strictEqual(invoice.payment_options.length, 1)
       assert(invoice.payment_options[0].outputs.length > 0) // it has at least a single output to the destination address
       assert.strictEqual(invoice.payment_options[0].outputs[0].address, 'qp2tqtmhm6ukrz0hnmxarqmdt2l4p38gdcwuxj40mr')
       assert.strictEqual(invoice.payment_options[0].currency_name, 'Bitcoin Cash')
