e9af368d4ac417f14a9d700dc3b3712f2d027935
Steven Zeiler <me@stevezeiler.com>
2021-01-24 12:53:12 -0500

    Automatically generate uid for grab and  go items

--- bin/grabandgo.ts
+++ bin/grabandgo.ts
@@ -5,8 +5,8 @@ import * as program from 'commander';
 import { models } from '../lib';
 
 program
-  .command('createitem <account_stub> <name> <price> <stub>')
-  .action(async (accountStub, name, price ,stub) => {
+  .command('createitem <email> <name> <price>')
+  .action(async (email, name, price) => {
 
     console.log('createitem');
 
@@ -15,21 +15,14 @@ program
 
       let account = await models.Account.findOne({
         where: {
-          stub: accountStub
+          email
         }
       });
 
-      let [item, isNew] = await models.GrabAndGoItem.findOrCreate({
-        where: {
-          stub,
-          account_id: account.id
-        },
-        defaults: {
-          stub,
-          name,
-          price,
-          account_id: account.id
-        }
+      let item = await models.GrabAndGoItem.create({
+        name,
+        price,
+        account_id: account.id
       });
 
       console.log('item created', item.toJSON());
