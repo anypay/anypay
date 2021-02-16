047616e9de3c4633a64714f2873bf3e8df46ee4d
Steven Zeiler <steven@anypayinc.com>
2021-02-16 15:37:38 -0500

    Automatically Create Default Platform API Key For Account (#833)
    Co-authored-by: Steven Zeiler <me@stevezeiler.com>

--- servers/rest_api/handlers/api_keys.ts
+++ servers/rest_api/handlers/api_keys.ts
@@ -3,12 +3,26 @@ import * as Boom from 'boom';
 
 import { apikeys } from '../../../lib'
 
+import { logError } from '../../../lib/logger'
+
 export async function index(req, h) {
 
-  let merchant_api_key = await apikeys.getMerchantApiKey(req.account.id)
+  try {
 
-  return {
-    merchant_api_key
+    let merchant_api_key = await apikeys.getMerchantApiKey(req.account.id)
+    let platform_api_key = await apikeys.getPlatformApiKey(req.account.id)
+
+    return {
+      merchant_api_key,
+      platform_api_key
+    }
+
+  } catch(error) {
+
+    logError('servers.rest_api.controllers.api_keys.index.error', error)
+
+    return Boom.badRequest(error.message)
+
   }
 
 }
