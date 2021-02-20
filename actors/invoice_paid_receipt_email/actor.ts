c8685fa7a3834167fb40de86d3114690d992d500
Steven Zeiler <steven@anypayinc.com>
2021-02-20 18:41:02 -0500

    Send Email On First Invoice Paid (#834)
    Co-authored-by: Steven Zeiler <me@stevezeiler.com>

--- actors/invoice_paid_receipt_email/actor.ts
+++ actors/invoice_paid_receipt_email/actor.ts
@@ -33,6 +33,42 @@ export async function start() {
 
   });
 
+  Actor.create({
+
+    exchange: 'anypay:invoices',
+
+    routingkey: 'invoice:paid',
+
+    queue: 'send_first_invoice_paid_email',
+
+  })
+  .start(async (channel, msg) => {
+
+    let uid = msg.content.toString();
+
+    let invoice = await models.Invoice.findOne({
+ 
+      where: { uid: uid }
+
+    });
+
+    let invoices = await models.Invoice.findAll({
+      where: {
+        account_id: invoice.account_id,
+        status: 'paid'
+      }
+    })
+
+    if (invoices.length === 1) {
+
+      await email.firstInvoicePaidEmail(invoice.toJSON());
+
+    }
+
+    channel.ack(msg);
+
+  });
+
 
 }
 
