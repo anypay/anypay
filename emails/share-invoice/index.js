0ffe5b266e0210a44de8b2148fedce5cc2615806
Steven Zeiler <me@stevezeiler.com>
2021-02-23 18:47:37 -0500

    Function & Template To Share Invoice Via Email

---
+++ emails/share-invoice/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const path_1 = require("path");
+const Handlebars = require("handlebars");
+let body = fs_1.readFileSync(path_1.join(__dirname, 'email.html'));
+let template = Handlebars.compile(body.toString());
+exports.default = {
+    title: "Anypay Invoice Received - Pay Now",
+    template,
+    body: template({})
+};
+//# sourceMappingURL=index.js.map
