d844e7bb8b541d3e80c2625f9b22c1374b711804
Steven Zeiler <steven@anypayinc.com>
2021-05-17 14:37:49 -0400

    Bittrex (#866)
    * Command + Library to Market Sell BSV on Bittrex
    
    * Auto-sell 1 BSV on Bittrex every minute
    
    Co-authored-by: Steven Zeiler <me@stevezeiler.com>

--- lib/bittrex/index.ts
+++ lib/bittrex/index.ts
@@ -1,6 +1,86 @@
-const { BittrexClient } = require('bittrex-node')
+require('dotenv').config()
+
+const CryptoJS = require('crypto-js')
+
+const API_BASE = 'https://api.bittrex.com/v3'
+
+import SHA512 from 'crypto-js/sha512'
+
+import * as http from 'superagent'
 
 export async function createOrder() {
+
+}
+
+export async function listOrders() {
+
+  let timestamp = new Date().getTime()
+
+  let contentHash = CryptoJS.SHA512('').toString(CryptoJS.enc.Hex)
+
+  let uri = `${API_BASE}/orders/open`
+
+  let method = 'GET'
+
+  var preSign = [timestamp, uri, method, contentHash].join('');
+
+  var signature = CryptoJS.HmacSHA512(preSign, process.env.BITTREX_SECRET).toString(CryptoJS.enc.Hex);
+
+  let headers = {
+    'Api-Key': process.env.BITTREX_KEY,
+    'Api-Timestamp': timestamp,
+    'Api-Content-Hash': contentHash,
+    'Api-Signature': signature
+  }
+
+  console.log('headers', headers)
+
+  return http.post(uri)
+    .set(headers)
+  
+}
+
+export async function marketOrder(quantity: number) {
+
+  let payload = {
+    "marketSymbol": "BSV-USD",
+    "direction": "sell",
+    "type": "market",
+    "quantity": quantity,
+    "timeInForce": "IMMEDIATE_OR_CANCEL"
+  }
+
+  let timestamp = new Date().getTime()
+
+  let contentHash = CryptoJS.SHA512(JSON.stringify(payload)).toString(CryptoJS.enc.Hex)
+
+  let uri = `${API_BASE}/orders`
+
+  let method = 'POST'
+
+  var preSign = [timestamp, uri, method, contentHash].join('');
+
+  var signature = CryptoJS.HmacSHA512(preSign, process.env.BITTREX_SECRET).toString(CryptoJS.enc.Hex);
+
+  let headers = {
+    'Api-Key': process.env.BITTREX_KEY,
+    'Api-Timestamp': timestamp,
+    'Api-Content-Hash': contentHash,
+    'Api-Signature': signature
+  }
+
+  console.log('headers', headers)
+  console.log('payload', payload)
+
+  let resp = await http.post(uri)
+    .set(headers)
+    .send(payload)
+
+  return resp.body
+  
+}
+
+export async function sellAllBSV() {
 
 }
 
