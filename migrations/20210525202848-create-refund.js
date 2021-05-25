269a73f1345ee34acf595218a0240965028b11cb
Steven Zeiler <me@stevezeiler.com>
2021-05-25 22:31:51 +0200

    Refund Model

---
+++ migrations/20210525202848-create-refund.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Refunds', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      invoice_uid: {
+        type: Sequelize.STRING
+      },
+      txid: {
+        type: Sequelize.STRING
+      },
+      rawtx: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Refunds');
+  }
+};
