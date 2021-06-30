95bcf417e694ae7c0d532e27f1698a822029aa77
Steven Zeiler <me@stevezeiler.com>
2021-06-30 20:55:32 +0200

    Auto Sell Everything Every Minute

--- actors/bittrex_autosell/actor.ts
+++ actors/bittrex_autosell/actor.ts
@@ -6,35 +6,27 @@ import { Actor, Joi, log } from 'rabbi';
 
 import { models } from '../../lib';
 
+import { logError } from '../../lib/logger'
+
 import * as cron from 'node-cron';
 
 import * as bittrex from '../../lib/bittrex'
 
 export async function start() {
 
-  cron.schedule('*/10 * * * * *', async () => { // every minute
+  cron.schedule('0 * * * * *', async () => { // every minute
 
     let bittrexAccounts = await models.BittrexAccount.findAll()
 
     for (let {account_id} of bittrexAccounts) {
 
-      let balances = await bittrex.listBalances(account_id)
+      try {
 
-      for (let balance of balances) {
+        bittrex.sellAll(account_id)
 
-        console.log('bittrex.balance', Object.assign(balance, {account_id}))
+      } catch(error) {
 
-        if (balance.available > 0) {
-
-          let order = await bittrex.sellAllOfCurrency(account_id, balance.currencySymbol)
-
-          if (order) {
-
-            console.log(`bittrex.order`, order)
-
-          }
-
-        }
+        logError('bittrex.sellall.error', error.message)
 
       }
 
