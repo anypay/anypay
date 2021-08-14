5a710778419090b825d8fcd0d7e6c4c2325a6d03
Steven Zeiler <me@stevezeiler.com>
2021-08-14 09:42:01 +0200

    Also update URI to type text

--- migrations/20210814065911-change-address-value-to-text.js
+++ migrations/20210814065911-change-address-value-to-text.js
@@ -3,9 +3,11 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
      await queryInterface.changeColumn('payment_options', 'address', { type: Sequelize.TEXT });
+     await queryInterface.changeColumn('payment_options', 'uri', { type: Sequelize.TEXT });
   },
 
   down: async (queryInterface, Sequelize) => {
      await queryInterface.changeColumn('payment_options', 'address', { type: Sequelize.STRING });
+     await queryInterface.changeColumn('payment_options', 'uri', { type: Sequelize.STRING });
   }
 };
