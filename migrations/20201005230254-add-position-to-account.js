83e9b7ae86574c1780aa5773a1a24f7a8237d242
Steven Zeiler <me@stevezeiler.com>
2021-11-06 12:30:32 +0100

    Add postgis extension to circleci test database

--- migrations/20201005230254-add-position-to-account.js
+++ migrations/20201005230254-add-position-to-account.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS postgis;');
     await queryInterface.addColumn('accounts', 'position', { type: Sequelize.GEOMETRY('POINT', 4326) });
   },
 
