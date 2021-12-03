dde5ee2eced92c3e8c65ef0ff3917c4ca0da44c7
Steven Zeiler <me@stevezeiler.com>
2021-12-03 06:28:56 +0100

    Update price cache in persist_prices actor

--- actors/persist_prices/actor.ts
+++ actors/persist_prices/actor.ts
@@ -6,13 +6,17 @@ import { Actor, Joi, log } from 'rabbi';
 
 import * as cron from 'node-cron'
 
-import { getCryptoPrices } from '../../lib/prices'
+import { getCryptoPrices, updateCryptoUSDPrices } from '../../lib/prices'
 
 export async function start() {
 
-  const interval = process.env.PERSIST_PRICES_INTERVAL || '0/10 * * * * *' // default to every ten minutes
+  await updateCryptoUSDPrices()
+
+  const interval = process.env.PERSIST_PRICES_INTERVAL || '0 */10 * * * *' // default to every ten minutes
 
   cron.schedule(interval, async () => {
+
+    await updateCryptoUSDPrices()
     
     await getCryptoPrices('USD')
 
