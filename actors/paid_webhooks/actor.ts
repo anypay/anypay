bc6c5188d1c783733ea1a9d4bb7adc286c679e4a
Steven Zeiler <me@stevezeiler.com>
2022-02-27 00:01:34 +0100

    Patch rabbi@1.18.2 to fix consumer issue

--- actors/paid_webhooks/actor.ts
+++ actors/paid_webhooks/actor.ts
@@ -5,8 +5,9 @@ import { log } from '../../lib';
 import { logInfo } from '../../lib/logger'
 
 import { Actor } from 'rabbi'
+//import { Actor } from '/Users/zyler/github/rabbijs/rabbi'
 
-import { Webhook, getPaidWebhookForInvoice } from '../../lib/webhooks';
+import { Webhook, getPaidWebhookForInvoice, sendWebhookForInvoice } from '../../lib/webhooks';
 
 import { ensureInvoice, Invoice } from '../../lib/invoices'
 
@@ -20,7 +21,7 @@ export async function start() {
 
     routingkey: 'invoice:paid',
 
-    queue: 'webhooks.invoice.tosend'
+    queue: 'webhooks.invoice.tosendpaid'
 
   })
   .start(async (channel, msg) => {
@@ -29,17 +30,33 @@ export async function start() {
 
       let uid = msg.content.toString();
 
+      logInfo('webhook.sendforinvoice', { uid });
+
       let invoice: Invoice = await ensureInvoice(uid)
+
+      logInfo('webhook.invoice', { invoice });
 
       let account: Account = await invoice.getAccount()
 
+      logInfo('webhook.account', { account });
+
       let webhook = await getPaidWebhookForInvoice(invoice)
+
+      logInfo('webhook', webhook)
 
       if (webhook) {
 
         let attempt = await webhook.attemptWebhook()
 
         logInfo('webhook.attempt', attempt.record.toJSON());
+
+      } else {
+
+        logInfo('webhook.sendforinvoice')
+
+        let result = await sendWebhookForInvoice(invoice.record, 'actor_onpaid')
+
+        logInfo('webhook.sendforinvoice.result', result)
 
       }
 
