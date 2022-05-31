3b4c20e85c170fcb66f83f40fa89bba84b44c910
Steven Zeiler <me@stevezeiler.com>
2022-06-01 00:28:00 +0200

    Include relational values for Kraken Trade records

--- migrations/20220531211646-add-tradeid-to-kraken-trade.js
+++ migrations/20220531211646-add-tradeid-to-kraken-trade.js
@@ -4,10 +4,12 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('KrakenTrades', 'tradeid', { type: Sequelize.STRING });
     await queryInterface.addColumn('KrakenTrades', 'account_id', { type: Sequelize.INTEGER });
+    await queryInterface.addColumn('KrakenTrades', 'date', { type: Sequelize.DATE });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('KrakenTrades', 'tradeid');
     await queryInterface.removeColumn('KrakenTrades', 'account_id');
+    await queryInterface.removeColumn('KrakenTrades', 'date');
   }
 };
