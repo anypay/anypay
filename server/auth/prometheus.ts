47946828c3c4b380ef2f4cccd8395884b4b5f547
Steven Zeiler <me@stevezeiler.com>
2022-06-12 08:44:46 +0200

    Report Prometheus Metrics Request Histograms

---
+++ server/auth/prometheus.ts
@@ -0,0 +1,64 @@
+
+import { validateAppToken } from './token'
+
+import { models } from '../../lib/models'
+
+import { log } from '../../lib/log'
+
+export async function auth(request, username, password, hapi) {
+
+  if (username.toLowerCase() !== 'prometheus') {
+    return {
+      isValid: false
+    }
+  }
+
+  let prometheusApp = await models.App.findOne({
+    where: {
+      name: 'prometheus',
+      account_id: 1177
+    }
+  })
+
+  log.debug('auth.app.prometheus', { username }) 
+
+  if (!password) {
+    return {
+      isValid: false
+    };
+  }
+
+  var accessToken = await models.AccessToken.findOne({
+    where: {
+      uid: password,
+      app_id: prometheusApp.id
+    }
+  });
+
+  if (!accessToken) {
+
+    log.error('auth.app.error', {
+      app_id: prometheusApp.id,
+      message: 'access token not found'
+    }) 
+
+    return {
+      isValid: false
+    }
+
+  }
+
+  return {
+
+    isValid: true,
+
+    credentials: {
+
+      accessToken
+
+    }
+
+  }
+
+}
+
