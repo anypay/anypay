cbb6834542f697694c6489866c7d049cd3951c30
Steven Zeiler <me@stevezeiler.com>
2022-07-26 23:31:31 +0200

    docs(include-swagger): include swagger module in server directory

---
+++ server/swagger.ts
@@ -0,0 +1,31 @@
+
+import * as Joi from '@hapi/joi'
+
+export const kBadRequestSchema = Joi.object({
+  statusCode: Joi.number().integer().required(),
+  error: Joi.string().required(),
+  message: Joi.string().required()
+}).label('BoomError')
+
+export function responsesWithSuccess({ model }) {
+  return {
+    'hapi-swagger': {
+      responses: {
+        200: {
+          description: 'Success',
+          schema: model
+        },
+        400: {
+          description: 'Bad Request',
+          schema: kBadRequestSchema,
+        },
+        401: {
+          description: 'Unauthorized',
+          schema: kBadRequestSchema,
+        },
+      },
+    },
+  }
+}
+
+
