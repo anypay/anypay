936ac9b476065400217d5ae75c66049adac17ef3
Zyler <508282+zylerdj@users.noreply.github.com>
2022-09-06 04:13:21 +0200

    (test) supress logs in tests

--- lib/log.ts
+++ lib/log.ts
@@ -5,6 +5,8 @@ import { models } from './models'
 
 import { publish } from './amqp'
 
+import { config } from './config'
+
 interface NewLogger {
   namespace: string;
 }
@@ -40,7 +42,11 @@ class Logger {
 
     }
 
-    this.log.info(type, payload)
+    if (config.get('NODE_ENV') !== 'test') {
+
+      this.log.info(type, payload)
+
+    }
 
     await publish(type, payload, 'anypay.topic')
 
