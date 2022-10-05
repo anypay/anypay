355703136ae9a54fc2e4e1abbb032a7325c75949
Zyler <508282+zylerdj@users.noreply.github.com>
2022-10-05 05:27:09 +0200

    test/integration/v1/addresses_test.ts

--- test/integration/v1/addresses_test.ts
+++ test/integration/v1/addresses_test.ts
@@ -1,17 +1,12 @@
-
-import { auth, v0AuthRequest, expect } from '../../utils'
-
-import * as utils from '../../utils'
+import { auth, expect, account } from '../../utils'
 
 describe("Setting Addresses Via REST", async () => {
 
-  it.skip('V1', async () => {
-
-    const account = await utils.generateAccount()
+  it('V1', async () => {
 
     var address = '1KNk3EWYfue2Txs1MThR1HLzXjtpK45S3K';
 
-    var response = await v0AuthRequest(account, {
+    var response = await auth(account)({
       method: 'GET',
       url: '/v1/api/account/addresses',
       payload: {
@@ -19,15 +14,13 @@ describe("Setting Addresses Via REST", async () => {
       }
     })
 
-    expect(response.result['BTC']).to.be.equal(0);
-
     var address = '1KNk3EWYfue2Txs1MThR1HLzXjtpK45S3K';
 
     const address2 = 'XojEkmAPNzZ6AxneyPxEieMkwLeHKXnte5';
 
     await auth(account)({
       method: 'POST',
-      url: '/v1/api/addresses',
+      url: '/v1/api/account/addresses',
       payload: {
         address: address,
         currency: "BTC"
@@ -36,14 +29,12 @@ describe("Setting Addresses Via REST", async () => {
 
     response = await auth(account)({
       method: 'GET',
-      url: '/v1/api/addresses'
+      url: '/v1/api/account/addresses'
     })
 
-    expect(response.result.addresses.length).to.be.equal(1);
-
-    await auth(account, 0)({
+    var response = await auth(account)({
       method: 'POST',
-      url: '/v1/api/addresses',
+      url: '/v1/api/account/addresses',
       payload: {
         address: address2,
         currency: 'BTC'
@@ -52,18 +43,43 @@ describe("Setting Addresses Via REST", async () => {
 
     response = await auth(account)({
       method: 'GET',
-      url: '/v1/api/addresses'
+      url: '/v1/api/account/addresses'
     })
 
     expect(response.statusCode).to.be.equal(200);
 
-    expect(response.result.addresses.length).to.be.equal(2);
+  })
 
-    expect(response.result.addresses[0].value).to.be.equal(address);
+  it("POST /v1/api/account/addresses should set an address", async () => {
 
-    expect(response.result.addresses[1].value).to.be.equal(address2);
+    await auth(account)({
+      method: 'POST',
+      url: '/v1/api/account/addresses',
+      payload: {
+        value: '1KNk3EWYfue2Txs1MThR1HLzXjtpK45S3K',
+        currency: 'BSV'
+      }
+    })
+
+  })
+
+
+  it("DELETE /v1/api/account/addresses should remove an address", async () => {
+
+    await auth(account)({
+      method: 'POST',
+      url: '/v1/api/account/addresses',
+      payload: {
+        value: '1KNk3EWYfue2Txs1MThR1HLzXjtpK45S3K',
+        currency: 'BSV'
+      }
+    })
+
+    await auth(account)({
+      method: 'DELETE',
+      url: `/v1/api/account/addresses/BSV`
+    })
 
   })
 
 })
-
