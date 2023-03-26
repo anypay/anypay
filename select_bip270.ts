aa5944d54f327cf03fa16d40c8ee11584c541d9b
Zyler <508282+zylerdj@users.noreply.github.com>
2023-03-26 02:42:48 +0100

    Test program to select bip270 locally

---
+++ select_bip270.ts
@@ -0,0 +1,51 @@
+
+import axios from 'axios'
+
+//const host = `http://localhost:5200`
+const host = `https://api.anypayx.com`
+
+export async function main() {
+
+
+    try {
+
+  const { data: invoiceData } = await axios.post(`${host}/invoices`, {
+    amount: 10 
+  }, {
+    auth: {
+      username: process.env.anypay_access_token,
+      password: ''
+    }
+  })
+
+  console.log(invoiceData)
+
+    const { data } = await axios.get(`${host}/i/${invoiceData.uid}`, {
+      headers: {
+        'accept': 'application/payment-options'
+      }
+    })
+
+    console.log(data)
+
+    const { data: options } = await axios.post(`${host}/i/${invoiceData.uid}`, {
+      currency: 'XMR',
+      chain: 'XMR'
+    }, {
+      headers: {
+        'content-type': 'application/payment-request'
+      }
+    })
+
+    console.log(options)
+
+  } catch(error) {
+
+    console.log(error)
+
+  }
+
+}
+
+main()
+
