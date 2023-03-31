f50087aaa7ac4a15a7ac476fd12351283fc6070c
Zyler <508282+zylerdj@users.noreply.github.com>
2023-03-31 10:15:00 +0000

    Solana Plugin with ./plugins/sol/bin.ts CLI

---
+++ plugins/sol/bin.ts
@@ -0,0 +1,108 @@
+#!/usr/bin/env ts-node
+
+require("dotenv").config()
+
+import { Command } from 'commander'
+
+const program = new Command()
+
+import { Keypair, Connection, PublicKey, Transaction } from "@solana/web3.js";
+
+import { createTransferInstruction, getOrCreateAssociatedTokenAccount } from '@solana/spl-token'
+
+import axios from 'axios'
+
+const SOLANA_MAINNET_USDC_PUBKEY = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
+//const SOLANA_MAINNET_USDT_PUBKEY = "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"
+
+const USDC = new PublicKey(SOLANA_MAINNET_USDC_PUBKEY)
+//const USDT = new PublicKey(SOLANA_MAINNET_USDT_PUBKEY)
+
+
+program
+  .command('pay-invoice <uid> <currency>')
+  .action(async (uid, currency) => {
+
+    const amount = 1_000_000 * 0.05
+
+    const connection = new Connection('https://solana-mainnet.g.alchemy.com/v2/zQCP8Bt8cAq63ToBYunRGWyag8HdzWp-');
+
+    const fromWallet = Keypair.fromSeed(Uint8Array.from(
+      JSON.parse(process.env.solana_phantom_seed).slice(0, 32)
+    ))
+
+    const fromAccountInfo = await connection.getAccountInfo(fromWallet.publicKey)
+
+    console.log(fromAccountInfo, 'fromAccountInfo')
+
+    const toWallet = new PublicKey('Ef9ca7Uwkw9rrbdaWnUrrdMZJqPYykZ1dPLEv9yMpEjB')
+
+    const toAccountInfo = await connection.getAccountInfo(toWallet);
+    console.log({ toAccountInfo })
+
+    // Create associated token accounts for my token if they don't exist yet
+    var fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      fromWallet, // payer
+      USDC, // mint
+      fromWallet.publicKey // owner
+    )
+    console.log({ fromTokenAccount })
+
+    var toTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      fromWallet,
+      USDC,
+      toWallet
+    )
+    console.log({ toTokenAccount }, toTokenAccount.address.toBase58())
+
+
+    const instruction = createTransferInstruction(
+      fromTokenAccount.address,
+      toTokenAccount.address,
+      fromWallet.publicKey,
+      amount
+    )
+
+    var transaction = new Transaction()
+
+    transaction.add(instruction)
+
+
+    transaction.feePayer = await  fromWallet.publicKey;
+    let blockhashObj = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = await blockhashObj.blockhash;
+
+    transaction.sign(fromWallet)
+
+    const txhex = transaction.serialize().toString('hex')
+
+    console.log({ txhex })
+
+    try {
+
+      const { data } = await axios.post(`http://localhost:5200/r/${uid}`, {
+        chain: 'SOL',
+        currency: 'USDC',
+        transactions: [{
+          tx: txhex
+        }]
+      }, {
+        headers: {
+          'content-type': 'application/payment'
+        }        
+      })
+
+      console.log(data, 'post response')
+
+    } catch(error) {
+
+      console.error(error)
+    }
+
+    
+
+  })
+
+program.parse(process.argv)
