4ba49146c076609c8c5d0ea529a94c79163012f3
Owen Kellogg <508282+owenkellogg@users.noreply.github.com>
2023-06-07 08:10:05 +0000

    Refactor requiredFeeRate and write test

--- test/lib/required_fee_rate_test.ts
+++ test/lib/required_fee_rate_test.ts
@@ -21,7 +21,7 @@ describe("Required Fee Rate", () => {
 
     const environmentVariable = process.env.REQUIRED_FEE_RATE_BSV
 
-    expect(environmentVariable).to.be.equal(undefined)
+    expect(environmentVariable).to.be.equal(null)
 
     const requiredFeeRate = await getRequiredFeeRate({ chain: 'BSV' })
 
