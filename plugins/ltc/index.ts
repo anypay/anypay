4bb96aebb33de4f4bc8d758fcf2b4e8b15166fd4
pshenmic <pshenmic@gmail.com>
2023-08-16 14:46:07 +0700

    Migrate LTC validation to bitcoinjs-lib (#1204)
    * Migrate LTC validation to bitcoinjs-lib
    
    * Clean up

--- plugins/ltc/index.ts
+++ plugins/ltc/index.ts
@@ -1,7 +1,7 @@
 
 import * as blockchair from '../../lib/blockchair'
 
-const ltc = require('litecore-lib');
+const bitcoinJsLib = require('bitcoinjs-lib')
 
 export const currency = 'LTC'
 
@@ -16,6 +16,19 @@ export default class LTC extends UTXO_Plugin {
   currency = 'LTC'
 
   chain = 'LTC'
+
+  networkInfo = {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bech32: 'ltc',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe,
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x32,
+    wif: 0xb0,
+  };
+
 
   decimals = 8
 
@@ -47,9 +60,9 @@ export default class LTC extends UTXO_Plugin {
 
     try {
 
-      new ltc.Address(address);
+      bitcoinJsLib.address.toOutputScript(address, LITECOIN)
 
-      return true;
+      return true
 
     } catch(error) {
 
