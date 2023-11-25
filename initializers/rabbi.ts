dcfdb5178d38a97053dc919e101233b0400452fa
My CTO <mycto@proton.me>
2023-11-25 21:20:26 +0100

    Log env in rabbi initializer

--- initializers/rabbi.ts
+++ initializers/rabbi.ts
@@ -4,6 +4,8 @@ import { getChannel } from 'rabbi'
 
 export default async function initialize() {
 
+  console.log(process.env) //@todo: remove
+
   const channel = await getChannel()
 
   await channel.assertExchange('anypay')
